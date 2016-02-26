f14867485727896ea6a51d330e551fa1562f897e
smithWEBtek <brad@smithwebtek.com>
2016-02-25 23:17:12 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
+
 class Dog
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood, :owner
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def self.owner=(owner_name)
+    @owner = owner_name
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,14 @@
+
 class Fish
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood, :owner
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def self.owner=(owner_name)
+    self.owner = owner_name
+  end
 end
