9fa3bf5bf1860198dea57fa2a90793bb52f476e0
johnram528 <johnram528@gmail.com>
2016-02-29 21:25:21 -0500

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  # code goes here
+  attr_reader :name 
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def happy
+    @mood = "happy"
+  end
+
+  def nervous
+    @mood = "nervous"
+  end
 end
