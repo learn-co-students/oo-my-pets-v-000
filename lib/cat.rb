c3cfb34c144c6ccb40d1e4a49505f3b145901de7
jmaerk235 <cmerrick@mail.fresnostate.edu>
2016-03-01 09:13:11 -0800

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_writer :mood
+  def initialize(name)
+    @mood = "nervous"
+    @name = name
+  end
+
+  def mood
+    @mood
+  end
+
+  #def new(name)
+  #  @pet = self.new(name)
+  #end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,14 @@
 class Fish
   # code goes here
+  attr_reader :name
+  attr_writer :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def mood
+    @mood
+  end
 end
