3b90fb0a1bb294256d2bd557b2087bd665b15f7e
jbok4 <jbok4@aol.com>
2016-03-04 19:08:54 -0500

    files initial setup

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,20 @@
 class Fish
-  # code goes here
+  
+  attr_reader :name
+  attr_accessor :mood, :owner
+
+  def initialize(name = "Nemo", mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+
+  def owner_name
+    if self.owner
+      self.owner.name
+    else
+      nil
+    end
+  end
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :owner, :fish, :cat, :dog, :pets
+
+  @pets = {fishes: [], cats: [], dogs: []}
+  @@owner_count = 0
+
+  def initialize(owner)
+    @owner = owner
+    @@owner_count += 1
+  end
+
+  def species(human)
+    @species = species
+    @species
+  end
+
+  def say_species
+    puts "#{species}"
+  end
+
+  def buy_fish(name)
+   fish = Fish.new(name)
+    @@pets[0] << fish
+    fish.owner = self
+  end
+
+  def list_pets
+    @pets.each do |k,v|
+      k.each do |pet|
+        puts '#{pet}'
+      end
+    end
+  end
+
+
 end
