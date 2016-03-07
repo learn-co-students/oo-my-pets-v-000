4c6539699b2b91ca11a209d450688c71b678bed2
jasonpaulso <hello@jasonpaulsouthwell.com>
2016-03-06 17:32:13 -0800

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,9 @@
 class Owner
   # code goes here
-  attr_accessor :name
+  attr_accessor :name, :pets
 
   def initialize(name)
     @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
   end
 end
