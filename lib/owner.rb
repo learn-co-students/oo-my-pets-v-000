928ff6159e9f660d8f4c4756e840bca2eadfe23a
tyleresmith <oats000@hotmail.com>
2016-03-09 02:56:45 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,54 @@
+require "pry"
+
 class Owner
   # code goes here
+  attr_accessor :pets, :name
+  attr_reader :species
+  @@all = []
+
+
+  def initialize(name="Bobby",species="human")
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = species
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |mood|
+      binding.pry
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.count
+  end
+
 end
