bca9aa6b4d3674ad73ba7369b15646b3ce6cdd73
CodeByLine <yleventhal@ymail.com>
2016-03-10 19:46:24 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class Cat
-  # code goes here
+
+    attr_accessor :mood, :count, :mood, :owner
+    attr_writer
+    attr_reader :name
+
+    @@all = []
+    count = 0
+
+    def initialize(name)
+        @name = name
+       # count += 1
+        @species = "cat"
+        @mood = "nervous"
+    end
+
+    def mood
+      @mood
+    end
+
+    def walking
+    end
+
+    def feeding
+    end
+
+    def playing
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+       @@count = 0
+    end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,38 @@
+require 'pry'
 class Dog
-  # code goes here
+
+    attr_accessor :mood, :count, :owner
+    attr_writer
+    attr_reader :name
+
+    @@all = []
+    count = 0
+
+    def initialize(name)
+        @name = name
+        @species = "dog"
+        @mood = "nervous"
+    end
+
+    def mood
+      @mood
+    end
+
+    def walking
+    end
+
+    def feeding
+    end
+
+    def playing
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    def self.reset_all
+       @@count = 0
+    end
 end
