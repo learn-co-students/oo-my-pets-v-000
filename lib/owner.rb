07562fcb245db04908e232322aabc0aa89a93666
jchu4483 <jchu4483@gmail.com>
2016-04-16 19:22:49 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -59,6 +59,10 @@ class Owner
         pet.mood = "nervous"
       end
         animals.clear
-      end
     end
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
 end
