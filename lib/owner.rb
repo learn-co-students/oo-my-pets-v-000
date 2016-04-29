945e3a3eb04b33f3a3e8d9b2f242d0e0d44a59c3
andytouch93 <andy@xavierdesignstudios.com>
2016-04-29 17:47:52 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,37 +27,37 @@ class Owner
   end
 
   def buy_fish(name)
-    pets[:fishes] << Fish.new(name)
+    self.pets[:fishes] << Fish.new(name)
   end
 
   def buy_dog(name)
-    pets[:dogs] << Dog.new(name)
+    self.pets[:dogs] << Dog.new(name)
   end
 
   def buy_cat(name)
-    pets[:cats] << Cat.new(name)
+    self.pets[:cats] << Cat.new(name)
   end
 
   def walk_dogs
-    pets[:dogs].each do |dog|
+    self.pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    pets[:cats].each do |cat|
+    self.pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
 
   def feed_fish
-    pets[:fishes].each do |fish|
+    self.pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
   end
 
   def sell_pets
-    pets.each do |pet_type, pets|
+    self.pets.each do |pet_type, pets|
       pets.each do |pet|
         pet.mood = "nervous"
       end
