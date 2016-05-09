0c2223a95f8e74a1e56963e6ec438d271143c4e1
kaileeagray <kaileeagray@gmail.com>
2016-05-08 22:01:38 -0500

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+
+  attr_reader :name, :mood
+
+def initialize(name)
+  @name = name
+end
+
+def mood=(mood = "nervous")
+  @mood = mood
+end
+
+def name=(name)
+    raise NewNameError, "Can't change the cat's name to #{name}. Cat's name is set to #{@name}." if @name != name
+end
+
+
 end
