7440bb6fba155a27b2df197ab03b3874c79fed44
kaileeagray <kaileeagray@gmail.com>
2016-05-08 22:17:29 -0500

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,14 @@
 class Fish
-  # code goes here
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def name=(name)
+    raise NewNameError, "Can't change the fish's name to #{name}. Fish's name is set to #{@name}." if @name != name
+  end
+
 end
