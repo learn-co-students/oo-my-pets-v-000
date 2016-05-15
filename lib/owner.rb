24f4a984c2a95779a1301fcc2fdb6caa53f959e5
kaileeagray <kaileeagray@gmail.com>
2016-05-15 12:59:40 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,4 @@
-require "pry "
+require 'pry'
 
 class Owner
   @@all = []
@@ -53,8 +53,20 @@ class Owner
   end
 
   def walk_dogs
+    @pets[:dogs].each {|dog| dog.mood="happy"}
+  end
+
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood="happy"}
+  end
+
+  def feed_fish
+    @pets[:fishes].each {|fish| fish.mood="happy"}
+  end
+
+  def sell_pets
     binding.pry
-    @pets[:dogs].each {|dog| dog.mood("happy")}
+    @pets.each {|species, pet| species.pet.mood = "nervous"}
   end
 
 
