1a488050fbba83abbeef3cd82d4ef444983fb5ef
steveafrost <ffrostt@gmail.com>
2016-05-22 00:18:15 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
-  # code goes here
+  
+  attr_accessor :mood
+  attr_reader :name
+  
+  def initialize(name_of_dog)
+    @name = name_of_dog
+    @mood = "nervous"
+  end
+  
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,11 @@
 class Fish
-  # code goes here
+  
+  attr_accessor :mood
+  attr_reader :name
+  
+  def initialize(name_of_fish)
+    @name = name_of_fish
+    @mood = "nervous"
+  end
+  
 end
