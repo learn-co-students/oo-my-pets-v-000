95f49b8b0ebd3d3a553c2627303e8375c73614f3
tpstar <hanlee12@gmail.com>
2016-05-30 07:59:33 -0500

    code OO my pets

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
   # code goes here
+  attr_reader :owner
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def name
+    @name.dup.freeze
+  end
+
 end
