d1c89ac7856fb68295d7bd9b8f1ee5b25bcbb2d2
bjacooper <bjacooper@gmail.com>
2016-06-09 16:56:44 +0100

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,60 @@
+require 'pry'
+
 class Owner
-  # code goes here
+
+  attr_reader :species
+  attr_accessor :name, :pets, :buy_fish
+
+  @@all = []
+
+  def initialize(species)
+    @@all << self
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def self.all 
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def say_species
+    "I am a human."
+  end
+
+
+
+
+  def buy_fish(fish_name)
+    Fish.all.detect do |f|
+      if f.name == fish_name
+        f
+      else
+        false
+      end
+      @pets[:fishes] << f
+    end
+  end
+
+
+
 end
+
+
+
+
+
+
+
+
+
+
+
+
