0ed2d26d965c47b3afa93e4079fa3e2701b4ae15
bjacooper <bjacooper@gmail.com>
2016-06-09 19:32:38 +0100

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Owner
 
   attr_reader :species
-  attr_accessor :name, :pets, :buy_fish
+  attr_accessor :name, :pets, :buy_dog
 
   @@all = []
 
@@ -32,24 +32,36 @@ class Owner
 
 
   def buy_fish(fish_name)
-    my_fish = Fish.all.detect {|f| f.name == fish_name}
-    @pets[:fishes] << my_fish if my_fish
-    binding.pry
+    fish = Fish.new(fish_name)
+    @pets[:fishes] << fish
+  end
+
+  def buy_cat(cat_name)
+    cat = Cat.new(cat_name)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(dog_name)
+    dog = Dog.new(dog_name)
+    @pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    @pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    @pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
 
-  def buy_fish(fish_name)
-    Fish.all.detect do |f|
-      if f.name == fish_name
-        f
-      else
-        false
-      end
-      @pets[:fishes] << f
-    end
+  def list_pets
+    "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
   end
-
-
 
 end
 
