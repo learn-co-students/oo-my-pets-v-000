482cd53891db777b137b9d9f474611e8383768af
Howard DeVennish <howarddevennish@gmail.com>
2016-07-28 20:27:24 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -31,41 +31,43 @@ class Owner
 
   def buy_fish(fish_name)
     new_fish = Fish.new(fish_name)
-    @pets[:fishes] << new_fish
+    pets[:fishes] << new_fish
 
   end
 
   def buy_cat(cat_name)
     new_cat = Cat.new(cat_name)
-    @pets[:cats] << new_cat
+    pets[:cats] << new_cat
   end
 
   def buy_dog(dog_name)
     new_dog = Dog.new(dog_name)
-    @pets[:dogs] << new_dog
+    pets[:dogs] << new_dog
   end
 
   def walk_dogs
-    @pets[:dogs].each {|dog| dog.mood = "happy"}
+    pets[:dogs].each {|dog| dog.mood = "happy"}
   end
 
   def play_with_cats
-    @pets[:cats].each {|cat| cat.mood = "happy"}
+    pets[:cats].each {|cat| cat.mood = "happy"}
   end
 
   def feed_fish
-    @pets[:fishes].each {|fish| fish.mood = "happy"}
+    pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
   def sell_pets
-    @pets.each do |pet_type, pet_arr|
+    pets.each do |pet_type, pet_arr|
       pet_arr.each {|pet| pet.mood = "nervous"}
+      pet_arr.clear
     end
-    @pets = {fishes: [], cats: [], dogs: []}
+
+
   end
 
   def list_pets
-    "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
   end
 
 end
