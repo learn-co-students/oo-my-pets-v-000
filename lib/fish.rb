3868ee4a25b0f581ee0f1790702f7916a3e1a189
KerryAlsace <kerrydougherty@gmail.com>
2016-08-02 14:39:11 -0400

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,10 +1,10 @@
 class Fish
-  attr_accessor :name
+  attr_accessor :mood
+  attr_reader :name
   
-  def initialize(name)
+  def initialize(name, mood = "nervous")
     @name = name
-    @mood = "nervous"
+    @mood = mood
   end
-
 
 end
