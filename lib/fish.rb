c976e6c577e2485b9272679bf3665edcc5ca2adf
Lani1321 <lani.berry13@gmail.com>
2016-08-12 11:26:25 -0400

    solution

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,14 @@
 class Fish
-  # code goes here
+  
+  attr_accessor :mood     #can change its mood
+
+  def initialize(name)    #can initialize a fish
+    @name = name
+    @mood = "nervous"     #initializes with a nervous mood
+  end
+
+  def name      #initializes with a name
+    @name 
+  end
+
 end
