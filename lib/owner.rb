b65dcd484043e50ee31bad75086d67aeed618bc1
Scott Savage <snsavage@gmail.com>
2016-08-14 16:04:47 -0400

    Passing OO My Pets Lab.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,72 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @pets = {fishes: [], dogs: [], cats: []}
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    @pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    @pets[:fishes].each {|fish| fish.mood = "happy"}
+  end
+
+  def sell_pets
+    @pets.each do |key, value|
+      value.each do |pet|
+        pet.mood = "nervous"
+      end
+    end
+    self.pets.clear
+  end
+
+  def list_pets
+    fish = pet_count(:fishes)
+    dogs = pet_count(:dogs)
+    cats = pet_count(:cats)
+    "I have #{fish} fish, #{dogs} dog(s), and #{cats} cat(s)."
+  end
+
+  def pet_count(species)
+    @pets[species].count
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.count
+  end
 end
