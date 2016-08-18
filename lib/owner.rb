13c813c24552c5cb7121da27d3cb41c8920a30ab
jack woodward <jackwoodwardschool@gmail.com>
2016-08-18 21:25:09 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,37 @@
 class Owner
   # code goes here
+  attr_accessor :pets, :name
+  attr_reader :species
+  @@all = []
+
+  def initialize(name)
+      @name = name
+      @species = species     #initializes owner species to be human
+      #@say_species = "I am a #{@species}."  #allows owner to say its species
+      @@all << self    #logs all instances of owners in @@all array
+      @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def self.all
+      @@all
+  end
+
+  def self.reset_all
+      @@all.clear
+  end
+
+  def self.count
+      @@all.length
+  end
+
+  def say_species
+   "I am a #{species}."
+  end
+
+  def buy_fish(name)
+      pets[:fishes] << Fish.new(name)
+  end
+
+
+
 end
