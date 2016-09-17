9a956e4b09336f7d5331067f31d8e970e8febc25
Will Lynch <will.lynch91@gmail.com>
2016-09-17 22:00:45 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,14 @@
 class Fish
   # code goes here
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+
+  def name=(name)
+    raise NoChangeError puts "Name already set to #{self.name}"
+  end
 end
