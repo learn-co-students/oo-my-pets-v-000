d92438eb8e50c71168ad2ee4edea37373f5f3d00
William Story <wstory@villanova.edu>
2016-10-05 20:41:14 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,23 @@
 class Dog
-  # code goes here
+
+  attr_accessor :name, :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def name=(name)
+    if @name != nil
+      message = "Can't change name."
+      raise Exception.new(message)
+    else
+      @name = name
+    end
+  end
+
+  def self.mood
+    @mood
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
+
+  attr_accessor :pets, :owner, :name, :species
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize(owner)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @owner = owner
+    @@all << owner
+    species
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.species=(species)
+    if @species != nil
+      message = "Can't change species."
+      raise Exception.new(message)
+    else
+      @species = species
+    end
+  end
+
 end
