c02870f857c54e814096f0e386a6d644d580464c
John Espinosa <johnfelix.espinosa@icloud.com>
2016-11-10 23:00:08 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,67 +1,68 @@
 class Owner
 
-	OWNERS = []
 	attr_accessor :name, :pets
 	attr_reader :species
 
-  def initialize(species)
-  	@species = species
-    OWNERS << self
-    @pets = {:fishes => [], :dogs => [], :cats => []}
-  end
+	@@all = []
 
-  def self.count
-  	OWNERS.size
-  end
+	def initialize(species)
+		@name = name
+		@species = species
+		@pets = {fishes: [], cats: [], dogs: []}
+		@@all << self
+	end
 
-  def self.all
-  	OWNERS
-  end
+	def self.all
+		@@all
+	end
 
-  def say_species
-    "I am a #{species}."
-  end
+	def self.count
+		@@all.count
+	end
 
-  def self.reset_all
-  	OWNERS.clear
-  end
+	def self.reset_all
+	  self.all.clear
+	end
 
-  def buy_fish(name)
-  	pets[:fishes] << Fish.new(name)
-  end
-    
-  def buy_cat(name)
-  	pets[:cats] << Cat.new(name)
-  end
+	def say_species
+		"I am a #{species}."
+	end
 
-  def buy_dog(name)
-  	pets[:dogs] << Dog.new(name)
-  end
+	def pets
+		@pets
+	end
 
-  def walk_dogs
-  	pets[:dogs].each {|dog| dog.mood = "happy"}
-  end
+	def buy_fish(name)
+		pets[:fishes] << Fish.new(name)
+	end
 
-  def play_with_cats
-  	pets[:cats].each {|cat| cat.mood = "happy"}
-  end
+	def buy_cat(name)
+		pets[:cats] << Cat.new(name)
+	end
 
-  def feed_fish
-  	pets[:fishes].each {|fish| fish.mood = "happy"}
-  end
+	def buy_dog(name)
+		pets[:dogs] << Dog.new(name)
+	end
 
-  def sell_pets
-  	pets.each {|species, pets| pets.each {|animal| animal.mood = "nervous"}}
-  	pets.clear
-  end
+	def walk_dogs
+		pets[:dogs].each {|dog| dog.mood = "happy"}
+	end
 
-  def list_pets
-    "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
-  end
+	def play_with_cats
+		pets[:cats].each {|cat| cat.mood = "happy"}
+	end
 
+	def feed_fish
+		pets[:fishes].each {|fish| fish.mood = "happy"}
+	end
+
+	def sell_pets
+    pets.each {|species, pets| pets.each {|animal| animal.mood = "nervous"}}
+		pets.clear
+	end
+
+	def list_pets
+		"I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+	end
 
 end
-
-
-
-
