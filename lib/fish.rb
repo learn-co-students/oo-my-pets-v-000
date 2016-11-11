d05e805ea57b0f54be9d4b4ba3d01ef3d4f94f27
Nicholas Ratti <nickratti@gmail.com>
2016-11-11 03:45:37 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,19 @@
 class Fish
   # code goes here
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def name
+    @name
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
 end
