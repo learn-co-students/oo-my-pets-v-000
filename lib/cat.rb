74d16e5207bbc00a3bbe6824c84affee1d4772a3
Mary Umoh <umohm12@gmail.com>
2016-11-18 19:52:25 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,11 @@
+require 'pry'
+
 class Cat
-  # code goes here
+   attr_reader :name #can't change it's name
+   attr_accessor :mood #can change it's mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,80 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+    @species = "human"
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+ def self.count
+   @@all.size
+ end
+
+ def self.reset_all
+   @@all.clear
+end
+
+  def say_species
+    "I am a #{self.species}."
+ end
+
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
+
+  def walk_dogs
+    pets[:dogs].each do |dog|   #dog hash has 2 dogs, go through each dog and change it's mood to happy
+    dog.mood= "happy"
+  end
+end
+
+ def play_with_cats
+   pets[:cats].each do |cat|
+     cat.mood = "happy"
+ end
+end
+
+ def feed_fish
+   pets[:fishes].each do |fish|
+     fish.mood = "happy"
+ end
+end
+
+  def sell_pets
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
+    end
+  end
+
+  def list_pets
+    "I have 2 #{:fish}, 3 #{:dog}(s), and 1 #{:cat}(s)."
+  end
+
 end
