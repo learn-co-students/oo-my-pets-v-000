4f0c5445a493a4ce1975524ebfbee4dd72749547
Mary Umoh <umohm12@gmail.com>
2016-11-18 19:57:03 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -74,7 +74,7 @@ end
   end
 
   def list_pets
-    "I have 2 #{:fish}, 3 #{:dog}(s), and 1 #{:cat}(s)."
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end
 
 end
