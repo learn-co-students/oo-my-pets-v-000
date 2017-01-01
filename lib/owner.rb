e29f03d3f26f0ed801469e260a16f2deba10a0c8
Ryan Evans <ryanwevans@icloud.com>
2016-12-31 16:51:14 -0800

    made code more efficient

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,10 +1,10 @@
 class Owner
   attr_accessor :name, :pets
+  attr_reader :species
   @@owners = []
 
-  def initialize(name)
-    @name = name
-    @species = "human"
+  def initialize(species)
+    @species = species
     @@owners << self
     @pets = {
       :fishes => [],
@@ -14,18 +14,15 @@ class Owner
   end
 
   def buy_fish(name)
-    new_fish = Fish.new(name)
-    @pets[:fishes] << new_fish
+    @pets[:fishes] << Fish.new(name)
   end
 
   def buy_cat(name)
-    new_cat = Cat.new(name)
-    @pets[:cats] << new_cat
+    @pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
-    new_dog = Dog.new(name)
-    @pets[:dogs] << new_dog
+    @pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
@@ -41,13 +38,21 @@ class Owner
   end
 
   def sell_pets
-    @pets[:dogs].each {|dog| dog.mood = "nervous" }
-    @pets[:cats].each {|cat| cat.mood = "nervous" }
-    @pets[:fishes].each {|fish| fish.mood = "nervous" }
+    # @pets[:dogs].each {|dog| dog.mood = "nervous" }
+    # @pets[:cats].each {|cat| cat.mood = "nervous" }
+    # @pets[:fishes].each {|fish| fish.mood = "nervous" }
+    #
+    # @pets[:dogs].clear
+    # @pets[:cats].clear
+    # @pets[:fishes].clear
 
-    @pets[:dogs].clear
-    @pets[:cats].clear
-    @pets[:fishes].clear
+    # More efficient code for my orignal code above:
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
+    end
   end
 
   def list_pets
@@ -66,12 +71,8 @@ class Owner
     @@owners.count
   end
 
-  def species
-    @species
-  end
-
   def say_species
-    "I am a human."
+    "I am a #{species}."
   end
 
 end
