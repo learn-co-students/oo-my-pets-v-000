2cd2c007c045b5e2f0653fd0c54804469873ed71
Ryan Evans <ryanwevans@icloud.com>
2016-12-31 16:27:34 -0800

    add related object methods

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def set_name(name)
+    self.name = name
+  end
+
+  def name
+    @name
+  end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,77 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  @@owners = []
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@owners << self
+    @pets = {
+      :fishes => [],
+      :dogs => [],
+      :cats => []
+    }
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes] << new_fish
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    @pets[:cats] << new_cat
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs] << new_dog
+  end
+
+  def walk_dogs
+    @pets[:dogs].each {|dog| dog.mood = "happy" }
+  end
+
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood = "happy" }
+  end
+
+  def feed_fish
+    @pets[:fishes].each {|fish| fish.mood = "happy" }
+  end
+
+  def sell_pets
+    @pets[:dogs].each {|dog| dog.mood = "nervous" }
+    @pets[:cats].each {|cat| cat.mood = "nervous" }
+    @pets[:fishes].each {|fish| fish.mood = "nervous" }
+
+    @pets[:dogs].clear
+    @pets[:cats].clear
+    @pets[:fishes].clear
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+  def self.count
+    @@owners.count
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    "I am a human."
+  end
+
 end
