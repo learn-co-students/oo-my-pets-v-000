c336639d364d80fbf9fb8305d23c03a31b2e63ec
Daniel Goldberg <danpaulgo@aol.com>
2017-01-09 00:21:23 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,27 @@
 class Cat
   # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @species = "cat"
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_or_create_by_name(name)
+    animal = @@all.detect do |cat|
+      cat.name == name
+    end
+    animal.nil? ? self.new(name) : animal
+  end
+
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,87 @@
+require "pry"
 class Owner
   # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets
+
+  @@all = []
+
+  def initialize(name)
+    @species = "human"
+    @name = name
+    @pets = {fishes: [], dogs: [], cats: []}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all = []
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def say_species
+    if species =~ /\A[aeiou]/
+      "I am an #{species}."
+    else
+      "I am a #{species}."
+    end
+  end
+
+  def buy_fish(name)
+    fish = Fish.find_or_create_by_name(name)
+    pets[:fishes] << fish
+  end
+
+  def buy_cat(name)
+    cat = Cat.find_or_create_by_name(name)
+    pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.find_or_create_by_name(name)
+    pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    pets.each_value do | pet_array |
+      pet_array.each do | pet |
+        pet.mood = "nervous"
+      end
+    end
+    @pets = {fishes: [], dogs: [], cats: []}
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+  end
+
 end
+
+dog = Owner.new("dog")
+puts dog.say_species
+octopus = Owner.new("octopus")
+puts octopus.say_species
