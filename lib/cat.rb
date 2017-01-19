85ed595ce010e854e78067b3edaa9d49bd8e4b35
Terry-Thompson <terry.lyn.thompson@gmail.com>
2017-01-19 13:31:58 -0600

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
 class Cat
-  # code goes here
+  attr_accessor :mood 
+  attr_reader :name, :species
+
+
+  def initialize(name, mood = "nervous")
+    @name = name 
+    @mood = mood
+  end# of initialize
+
+
 end
