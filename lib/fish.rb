c27050debc9ab4e10d3049bd7f4d97acf2168b44
Elias Ventura <eliasventura23@gmail.com>
2017-01-27 22:08:32 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,24 +1,10 @@
 class Fish
 
-<<<<<<< HEAD
   attr_accessor :mood
   attr_reader :name
 
   def initialize(name)
     @name = name
     @mood = "nervous"
-
   end
-=======
-
-    attr_accessor :mood
-
-    attr_reader :name
-
-    def initialize(name)
-      @name = name
-      @mood = "nervous"
-    end
-
->>>>>>> 73e687bce1cd8f5f2ee1fefc87238184a1c33634
 end
