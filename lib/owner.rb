5055595fc79e486c1711134bf1d79282592f716d
Kevin Adkins <kevinladkins@gmail.com>
2017-01-28 01:32:41 +0000

    built Owner initialize and @@all methods

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,14 +5,26 @@ class Owner
 
   @@all = []
 
-  def all
-    @@all.dup.freeze
-  end
-
   def initialize(species)
     @species = species
     @pets = {:fishes => [], :cats => [], :dogs => []}
     @@all << self
+  end
+
+  def say_species
+    puts "I am a #{@species}"
+  end
+
+  def self.all
+    @@all.dup.freeze
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def self.reset_all
+    @@all.clear
   end
 
   def walk_dogs
