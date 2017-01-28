70ae1f4b297524d11993fb1fa859af67dd1c6045
Kevin Adkins <kevinladkins@gmail.com>
2017-01-28 01:22:58 +0000

    created cat/dog/fish classes

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
   # code goes here
+  attr_accessor :name
+  attr_reader :species
+
+  @@all = []
+
+  def all
+    @@all.dup.freeze
+  end
+
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :cats => [], :dogs => []}
+    @@all << self
+  end
+
+  def walk_dogs
+
+  end
+
+  def play_with_cats
+
+  end
+
+  def feed_fish
+
+  end
+
 end
