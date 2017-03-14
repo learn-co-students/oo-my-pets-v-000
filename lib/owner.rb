03dfdfe50ea5d5dbc8c4cc7ad861289c4748d9be
Seth Alexander <seth.a.alexander@gmail.com>
2017-03-14 03:08:51 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,68 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all =[]
+
+  def initialize(name)
+    @name = name
+    @@all << self
+    @species = "human"
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    @pets[:dogs].each {|d| d.mood = "happy"}
+  end
+
+  def play_with_cats
+    @pets[:cats].each {|c| c.mood = "happy"}
+  end
+
+  def feed_fish
+    @pets[:fishes].each {|f| f.mood = "happy"}
+  end
+
+  def sell_pets
+    @pets.each_value {|s| s.each {|p| p.mood = "nervous"}.clear}
+  end
+
+  def list_pets
+    dogs = @pets[:dogs].length
+    cats = @pets[:cats].length
+    fish = @pets[:fishes].length
+    "I have #{fish} fish, #{dogs} dog(s), and #{cats} cat(s)."
+  end
+
 end
