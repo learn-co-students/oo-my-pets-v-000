2458372b9e79dd49d3bc1a0f45fbabf270c1498d
Jessica Widman <jawidman.30@gmail.com>
2017-03-16 04:00:47 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,21 @@
 class Dog
-  # code goes here
+  attr_accessor :name, :mood
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def name=(name)
+    if @name.nil?
+      @name = name
+    else
+      raise 'Error: cannot change name'
+    end
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
 end
