888f0f461f1323facaba9ebb5bca8b5f87cedb18
irina <iafanasyevaa@gmail.com>
2017-04-10 19:47:08 +0300

    unfinished result

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,12 @@
 class Owner
-  # code goes here
+  attr_writer :pets
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+  end
+
+  def pets
+    @pets.dup.freeze
+  end
 end
