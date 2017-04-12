e5f9951a9172f07bd369513ec70f26ad1eb5eeee
Eva Navarror <emanao@me.com>
2017-04-12 19:52:29 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,6 +1,11 @@
 class Cat
- attr_accessor :name
- def initialize(name)
-   @name=name
- end
+  attr_accessor :mood
+  attr_reader :name
+  def initialize(name)
+    @name=name
+    @mood="nervous"
+  end
+  def name=(name)
+    raise SettingWriterError,"#{self.name} received, none expected."
+  end
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  def initialize(name)
+    @name=name
+    @mood="nervous"
+  end
+  def name=(name)
+    raise SettingWriterError,"#{self.name} received, none expected."
+  end
 end
