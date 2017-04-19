12d2afb78e894bba8550bc9073958964c22ba99c
Carolyn Yates <yatescjnd@gmail.com>
2017-04-19 02:32:52 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,11 @@
 class Cat
 
-  attr_accessor :name
+  attr_accessor :mood
+  attr_reader :name
 
-  def initialize(name)
+  def initialize(name, mood = "nervous")
     @name = name
+    @mood = mood
   end
 
 
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,97 @@
+require 'pry'
 class Owner
-  # code goes here
+
+  attr_reader :species #:name
+  attr_accessor :pets, :name
+  @@all = []
+  @@count = 0
+
+  def initialize(species)
+    @pets = {fishes: [], dogs: [], cats: []}
+    @species = species
+    @@all << self
+    @@count =+1
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@count
+  end
+
+  def self.reset_all
+    @@all.clear
+    @@count = 0
+  end
+
+  def species
+    @species
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    @pets[:dogs].each {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    @pets[:fishes].each {|fish| fish.mood = "happy"}
+  end
+
+
+  def sell_pets
+    @pets.each do |species, animals|
+      animals.each {|animal| animal.mood = "nervous"}
+      animals.clear
+    end
+  end
+
+  def list_pets
+    start_string= "I have"
+    @pets.each do |species, animals|
+      if species.to_s == "fishes"
+        species = "fish"
+        start_string += " #{animals.count} #{species},"
+      elsif species.to_s == "dogs"
+        species = "dog(s)"
+        start_string += " #{animals.count} #{species},"
+      else
+        species = "cat(s)"
+        start_string += " and #{animals.count} #{species}."
+      end
+    end
+      start_string
+  end
 end
+#       # else
+#       #   species = species.to_s.chop + "(s)"
+#       end
+#        start_string +#= " #{animals.count} #{species}"
+#     end
+#     start_string
+#   end
+#
+# end
