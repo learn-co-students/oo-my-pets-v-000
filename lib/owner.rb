a23222fca2b4de7440f64764a0a31641632665ca
Nick Bair <bairinc@gmail.com>
2017-04-30 21:26:37 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,75 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  @@all = []
+
+  # Class Methods
+  def initialize(species)
+    @species = species
+    @pets = {
+      fishes: [],
+      dogs: [],
+      cats: []
+    }
+
+    self.save
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  # Instance Methods
+  def save
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    @pets[:dogs].each { |dog| dog.mood = "happy" }
+  end
+
+  def play_with_cats
+    @pets[:cats].each { |cat| cat.mood = "happy" }
+  end
+
+  def feed_fish
+    @pets[:fishes].each { |fish| fish.mood = "happy" }
+  end
+
+  def sell_pets
+    self.pets.keys.map do |species|
+      self.pets[species].map { |pet| pet.mood = "nervous"  }
+    end
+
+    self.pets.keys.map { |e| self.pets[e].clear  }
+  end
+
+  def list_pets
+    pets = self.pets
+    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+  end
 end
