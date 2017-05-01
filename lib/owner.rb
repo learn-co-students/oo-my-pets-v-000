0688049980679f1472c3dd46a91fee5159997d1e
Will-Sauer <will@heueso.com>
2017-05-01 14:44:08 -0700

    finished

--- lib/owner.rb
+++ lib/owner.rb
@@ -66,4 +66,43 @@ obj_type = "Cat"
 @pets[:cats] << Kernel.const_get(obj_type).new(name)
 end
 
+def walk_dogs
+pet_key = :dogs
+self.pets[pet_key].each{|pet| pet.mood = "happy"}
+end
+
+
+def play_with_cats
+pet_key = :cats
+self.pets[pet_key].each{|pet| pet.mood = "happy"}
+end
+
+
+def feed_fish
+pet_key = :fishes
+self.pets[pet_key].each{|pet| pet.mood = "happy"}
+end
+
+
+def sell_pets
+#goes through and makes them all nervous, then removes them from owner
+@pets.each{|type,pets|
+                      pets.each{|indiv_pet| indiv_pet.mood = "nervous"}
+          pets.clear
+          
+      }
+end
+
+def list_pets
+pet_list = "I have "
+pet_list += "#{pets[:fishes].count} fish, "
+pet_list += "#{pets[:dogs].count} dog(s), "
+pet_list += "and #{pets[:cats].count} cat(s)."
+
+pet_list
+end
+
+
+
+
 end
