9d2a8891a94b88ecd91bc68f7b7d0d46755d70e1
hyoyou <heatherhyou@gmail.com>
2017-05-02 15:17:37 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -29,42 +29,42 @@ class Owner
   end
 
   def buy_fish(name)
-    @pets[:fishes] << Fish.new(name)
+    pets[:fishes] << Fish.new(name)
   end
 
   def buy_cat(name)
-    @pets[:cats] << Cat.new(name)
+    pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
-    @pets[:dogs] << Dog.new(name)
+    pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
-    @pets[:dogs].map do |dog|
+    pets[:dogs].each do |dog|
       dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    @pets[:cats].map do |cat|
+    pets[:cats].each do |cat|
       cat.mood = "happy"
     end
   end
 
   def feed_fish
-    @pets[:fishes].map do |fish|
+    pets[:fishes].each do |fish|
       fish.mood = "happy"
     end
   end
 
   def sell_pets
-    @pets.each do |pet, details|
-      details.collect do |animal|
+    pets.each do |pet, details|
+      details.each do |animal|
         animal.mood = "nervous"
       end
     end
-    @pets.clear
+    pets.clear
   end
 
   def list_pets
