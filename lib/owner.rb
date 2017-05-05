804717b99d01394f18c5c19697824f50d040886d
Julie Widman <juliewdmn@gmail.com>
2017-05-05 15:28:43 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
-  # code goes here
+  attr_accessor :owner
+
+  @@all = []
+
+  def initialize(owner)
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def save
+    @@all << self
+  end
+
+ def self.count
+   @@all.count
+ end
+
+  def self.reset_all
+    @@all.clear
+  end
 end
