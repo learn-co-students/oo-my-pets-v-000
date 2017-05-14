0904c0df06070b3dbbe79892b37b2c3690c2845e
Samantha Knight <sammay82@yahoo.com>
2017-05-14 19:07:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
   # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  
+  def initialize(name, mood="nervous")
+    @name = name
+    @mood = mood
+  end
+
+  def self.all
+    @@owners
+  end
+
+
+
 end
