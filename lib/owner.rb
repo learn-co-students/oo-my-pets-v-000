5516b090ea7a47439dd52060fd37651ff1942072
Christina Varipapa <christina.varipapa@gmail.com>
2017-05-25 03:39:03 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -44,20 +44,34 @@ class Owner
   end
 
   def walk_dogs
-    @pets.dogs.@mood = "happy"
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
   end
 
   def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
   end
 
   def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
   end
 
   def sell_pets
+    @pets.each do |species, individuals|
+      individuals.each do |pet|
+        pet.mood = "nervous"
+      end
+      individuals.clear
+    end
   end
 
   def list_pets
-    @@pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
 
   def self.reset_all
