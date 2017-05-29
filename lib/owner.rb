8df5788eeb694e1ca8fc6dc2c51a3aa42e988a59
Samson Yuwono <samson.yuwono@gmail.com>
2017-05-29 21:30:36 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@owners = []
+  @@owner_count= 0
+
+  def initialize(name)
+    @name= name
+    @@owners << self
+    @@owner_count +=1
+    @pets = {fishes: [], dogs: [], cats: [] }
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def reset_all
+    @@owners.clear
+  end
+
+
 end
