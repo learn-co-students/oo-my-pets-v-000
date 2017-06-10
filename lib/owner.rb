c69b5a5d50b0a6856c70fd7d92d1ece14f2c6084
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-06-10 19:27:45 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,21 @@
+require 'pry'
 class Owner
   # code goes here
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  attr_accessor :species
+
+  def initialize(species)
+    @species = species
+    @@all << self 
+  end
+
+  def self.create
+    @@all << self.new
+  end
+
 end
