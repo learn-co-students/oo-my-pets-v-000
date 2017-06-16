34decaf17315148273a5b2a4ffe41df4f7590218
Timothy Cevallos <timcev@gmail.com>
2017-06-16 17:44:26 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,10 @@
 class Fish
+   attr_accessor :mood
+    attr_reader :name
+    # code goes here
+    def initialize(name)
+      @name = name
+      @mood = "nervous"
+    end
   # code goes here
 end
