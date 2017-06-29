e4cc8dd42bf5b6221d890ae39c29dfc6b3a66b3b
Yujun Sung <yujunsung@gmail.com>
2017-06-29 19:01:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
+  attr_accessor :mood, :owner
+
   # code goes here
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+  def name
+    @name.dup.freeze
+  end
+
 end
