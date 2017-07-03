599dd28eee321781c8695621dd0736bc3b7842bb
lpassamano <leighayna.josele@gmail.com>
2017-07-03 14:58:32 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -19,49 +19,49 @@ class Owner
 
   def buy_fish(name)
     new_fish = Fish.new(name)
-    self.pets[:fishes] << new_fish
+    pets[:fishes] << new_fish
   end
 
   def feed_fish
-    self.pets[:fishes].collect {|fish| fish.mood = "happy"}
+    pets[:fishes].collect {|fish| fish.mood = "happy"}
   end
 
   def buy_cat(name)
     new_cat = Cat.new(name)
-    self.pets[:cats] << new_cat
+    pets[:cats] << new_cat
   end
 
   def play_with_cats
-    self.pets[:cats].collect {|cat| cat.mood = "happy"}
+    pets[:cats].collect {|cat| cat.mood = "happy"}
   end
 
   def buy_dog(name)
     new_dog = Dog.new(name)
-    self.pets[:dogs] << new_dog
+    pets[:dogs] << new_dog
   end
 
   def walk_dogs
-    self.pets[:dogs].collect {|dog| dog.mood = "happy"}
+    pets[:dogs].collect {|dog| dog.mood = "happy"}
   end
 
   def list_pets
-    "I have #{self.pets[:fishes].length} fish, #{self.pets[:dogs].length} dog(s), and #{self.pets[:cats].length} cat(s)."
+    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
   end
 
   def sell_pets
     #change mood on all pets to "nervous"
-    self.pets.each_pair do |key, pets_array|
-      self.pets[key].each {|pet| pet.mood = "nervous"}
+    pets.each_pair do |key, pets_array|
+      pets[key].each {|pet| pet.mood = "nervous"}
     end
-    self.pets.clear
+    pets.clear
   end
 
   def self.reset_all
-    self.all.clear
+    all.clear
   end
 
   def self.count
-    self.all.length
+    all.length
   end
 
 end
