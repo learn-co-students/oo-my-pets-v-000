22f645242dc21b77574a046e5c5fd14abef054d2
haroon Azhar Khan <haroon1azhar@gmail.com>
2017-07-19 17:54:23 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
+  attr_reader :name, :mood
   # code goes here
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+  def mood=(mood)
+    @mood=mood
+  end
 end
--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,11 @@
 class Fish
+  attr_reader :name, :mood
   # code goes here
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+  def mood=(mood)
+    @mood=mood
+  end
 end
