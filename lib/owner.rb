60001eb43c64478d02277e59f3eb766eb8078484
churchwerks <steven.lennis@icloud.com>
2017-08-10 23:44:35 -0500

    all tests passing except pwner class

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,16 @@
+require "pry"
 class Owner
-  # code goes here
+  attr_accessor :owner, :pets
+  @@all = {}
+
+  def initialize(owner)
+    @owner = owner
+    @pets = {fishes: [], cats: [], dogs: [] }
+
+  end
+
+  def self.all
+    @@all = {}
+  end
+
 end
