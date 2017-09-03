9a2a4bdeaf98ba83d1f6a22a6b4ebc2beb74ec0e
Susan Fowler <infopro94@gmail.com>
2017-09-03 23:45:06 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,12 @@
 class Fish
-  # code goes here
+  attr_accessor :name, :mood
+
+  def initialize(name)
+    @name = name
+  end
+
+  # def mood
+  #   self.mood = nervous
+  # end
+
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,50 @@
 class Owner
-  # code goes here
+  attr_accessor :owner, :pets, :species
+
+  def initialize(name)
+    @name = name
+    @pets = {:fishes=>[], :dogs=>[], :cats=>[]}
+    @species
+  end
+
+  def species
+
+  end
+
+  def say_species
+    # self.say_species
+    "I am a human."
+  end
+
+  def name
+    @name = name
+  end
+
+  def buy_fish(pet_name)
+    # self.buy_fish(pet_name)
+  end
+
+  def buy_cat(pet_name)
+    # self.buy_cat(pet_name)
+  end
+
+  def buy_dog(pet_name)
+    # self.buy_dog(pet_name)
+  end
+
+  def walk_dogs
+  end
+
+  def play_with_cats
+  end
+
+  def feed_fish
+  end
+
+  def sell_pets
+  end
+
+  def list_pets
+  end
+
 end
