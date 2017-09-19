d5a4ed25e7b1b347c3069b10d57333e88294eabe
matos89 <matos89@gmail.com>
2017-09-18 21:13:17 -0700

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 class Owner
 
-  attr_accessor :pets, :name
+  attr_accessor :pets, :name, :sentence
   attr_reader :species
   @@all = []
 
@@ -45,8 +45,42 @@ class Owner
   end
 
   def walk_dogs
-    dog.mood= "happy"
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
   end
 
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.each do |animal, pets|
+      pets.each do |pet|
+        pet.mood = "nervous"
+      end
+    end
+    @pets.clear
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
+
+  # trying to scale list_pets incase there are a lot of pets, not successful yet.
+  # def list_pets
+  #   sentence = []
+  #   @pets.each do |animal, pets|
+  #     sentence << "I have #{pets.count} #{animal.to_s.chomp("s")}(s)"
+  #   end
+  #   sentence.join(", and ")
 
 end
