a2fc318aef4cb9f529c06aa196e698dd0b873397
joey michalina mariano <joey.michalina.mariano@gmail.com>
2017-09-29 17:41:29 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,5 @@
 class Owner
-  attr_accessor :name
+  attr_accessor :name, :pets
   attr_reader :species
   @@all = []
 
@@ -12,17 +12,17 @@ class Owner
 
   def buy_fish(name)
     new_fish = Fish.new(name)
-    @pets[:fishes] = [].concat([new_fish])
+    @pets[:fishes] << new_fish
   end
 
   def buy_cat(name)
     new_cat = Cat.new(name)
-    @pets[:cats] = [].concat([new_cat])
+    @pets[:cats] << new_cat
   end
 
   def buy_dog(name)
     new_dog = Dog.new(name)
-    @pets[:dogs] = [].concat([new_dog])
+    @pets[:dogs] << new_dog
   end
 
   def say_species
@@ -42,18 +42,16 @@ class Owner
   end
 
   def sell_pets
-    # @pets.each do |key|
-    #   key.each do |array|
-    #     array.each do |index|
-    #       index.mood = "nervous"
-    #       index.delete
-    #     end
-    #   end
-    # end
+    @pets.each do |key, array|
+      array.each do |index|
+        index.mood = "nervous"
+      end
+      array.clear
+    end
   end
 
-  def pets
-    @pets
+  def list_pets
+    phrase = "I have #{@pets[:fishes][0..-1].length} fish, #{@pets[:dogs][0..-1].length} dog(s), and #{@pets[:cats][0..-1].length} cat(s)."
   end
 
   def self.all
