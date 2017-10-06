e2dd9d62c47a9df62baaae8ac11611751b1b9b27
Lisa Lee <lisa_lee@brown.edu>
2017-10-06 17:21:11 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,8 +34,35 @@ class Owner
   def walk_dogs
     @pets[:dogs].each do |dog|
       dog.mood = "happy"
-    end 
-  end 
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.each do |keys, values|
+      values.each do |pet|
+        pet.mood = "nervous"
+
+      end
+    end
+    self.pets.clear
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end
+
   def self.all
     @@all
   end
