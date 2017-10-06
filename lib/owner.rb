d324ac2509b10e10da3f80ef5e9a7df6f8ddc883
Lisa Lee <lisa_lee@brown.edu>
2017-10-06 17:05:52 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,52 @@
 class Owner
-  # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets
+  @@all = []
+
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {
+      fishes: [],
+      dogs: [],
+      cats: []
+    }
+  end
+
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end 
+  end 
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+
+
 end
