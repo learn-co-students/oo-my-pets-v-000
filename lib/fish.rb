0da6203998647cfdb0be6bb758b6ec6536959f7c
Emily Sugano <emily.sugano@gmail.com>
2017-10-18 19:51:51 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
 class Fish
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    self.mood = "nervous"
+  end
+
+  def mood
+    @mood
+  end
 end
