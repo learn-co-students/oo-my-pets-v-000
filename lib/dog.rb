2944041ad7ccc546be19c789a30a0c2219f35498
github username <wigmore3.14@gmail.com>
2017-10-30 15:21:56 -0700

    My pets - Done

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,11 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  # initialize with name and default mood
+  def initialize(name, mood="nervous")
+    @name = name
+    @mood = mood
+  end
+
 end
