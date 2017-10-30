e3b0886436fa143d117d6cb6eacf6736d191c1e2
Kevin Huang <yellow.khaos@gmail.com>
2017-10-30 03:05:27 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,18 @@
 class Dog
+    attr_reader :name
+    attr_accessor :mood, :owner
+
+    def initialize(name)
+      @name = name
+      @mood = "nervous"
+    end
+
+    def owner=(new_owner)
+      if new_owner.is_a? Owner
+        if !(@owner == new_owner)
+          @owner = new_owner
+        end
+      end
+    end
   # code goes here
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,82 @@
 class Owner
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.all
+    @@all
+  end
+
+  def initialize(species_owner)
+    @species = species_owner
+    @pets = {:fishes=>[], :dogs =>[], :cats => []}
+    @@all<<self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(name)
+    new_fish = Fish.new(name)
+    @pets[:fishes]<<new_fish
+    if !(new_fish.owner == self)
+      new_fish.owner = self
+    end
+  end
+
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    @pets[:cats]<<new_cat
+    if !(new_cat.owner == self)
+      new_cat.owner = self
+    end
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    @pets[:dogs]<<new_dog
+    if !(new_dog.owner == self)
+      new_dog.owner = self
+    end
+  end
+
+  def walk_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.each do |type, pets|
+      pets.each {|pet|pet.mood="nervous"}
+    end
+    @pets.each {|type, pets|pets.clear}
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
   # code goes here
 end
