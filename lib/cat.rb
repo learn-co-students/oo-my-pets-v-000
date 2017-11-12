ed7e900b79d78d4d4d08d0dc05e524c3408653f7
github username <github email address>
2017-11-12 15:31:08 -0600

    second commit

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+  
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    self.mood = "nervous"
+  end
+
+  def add_a_cat
+    raise ImproperSentienceActionError if !name.is_a?(Cat)
+  end
+
 end
