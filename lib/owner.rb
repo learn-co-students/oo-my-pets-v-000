c714782ba039f3c8f5d7a04acce17e25b880315c
github username <github email address>
2017-11-12 15:05:03 -0600

    first commit

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :pets
+
+  def initialize
+    @pets = {fishes: [], 
+            cats: [], 
+            dogs: []}
+  end
+
+  def buy_cat(name)
+    name = cat.new
+  end
+
+  def buy_dog(name)
+    name = dog.new
+  end
+
+  def buy_fish(name)
+    name = fish.new
+  end
+
 end
