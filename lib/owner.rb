85d4e848544fe7e64f43bc5d200b8042a47badbd
Christopher DiStasio <github email address>
2017-11-13 15:20:01 -0600

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -19,15 +19,18 @@ class Owner
   end
 
   def buy_cat(name)
-    name = cat.new
+    cat = Cat.new(name)
+    @pets[:cats] << cat
   end
 
   def buy_dog(name)
-    name = dog.new
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
   end
 
   def buy_fish(name)
-    name = fish.new
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
   end
 
   def self.all
@@ -50,18 +53,27 @@ class Owner
     return "I am a #{@species}."
   end
 
-  def buy_fish
-    # if the owner buys a fish, it will go into the array of values for the fish key
-    # and each fish that's added will be an instance of the Fish class
-    
-    #it 'can buy a fish that is an instance of the Fish class' do 
-    #     expect(owner.pets[:fishes].count).to eq(0)
-    #     owner.buy_fish("Bubbles")
-    #     owner.pets[:fishes].each do |fish|
-    #       expect(fish).to be_a(Fish)
-    #     end
-    #     expect(owner.pets[:fishes].count).to eq(1)
-    #   end
+  def walk_dogs
+    @pets[:dogs].each {|dog| dog.mood = "happy"}
   end
 
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+    
+  def feed_fish
+    @pets[:fishes].each {|fish| fish.mood = "happy"}
+  end
+
+  def sell_pets
+    @pets[:cats].each {|cat| cat.mood = "nervous"}
+    @pets[:dogs].each {|dog| dog.mood = "nervous"}
+    @pets[:fishes].each {|fish| fish.mood = "nervous"}
+    @pets.clear        
+  end
+
+  def list_pets
+    return "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end
+    
 end
