c85a49c2f67d541da278c62fdd9425d4cc7f47b4
Olena Ageyeva <eageeva@hotmail.com>
2017-11-16 03:50:17 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
   # code goes here
+  attr_accessor :name, :mood
+  def initialize(name="", mood="")
+    @name=name
+    @mood="nervous"
+  end
+
+  def raise_exception
+    raise 'An error has occured'
+  end
+
+  def name=(name)
+    raise_exception
+  end
 end
