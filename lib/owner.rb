ab086ed5073610b72b642f742c08aa025af97bfd
Brittany Hartmire <bkhartmire@gmail.com>
2017-11-23 20:26:14 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -54,7 +54,12 @@ class Owner
   end
 
   def sell_pets
-    self.pets.values.each {|pet| pet.mood = "nervous"}
+    self.pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
+    end
   end
 
   def list_pets
