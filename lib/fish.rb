e5921f379b2bef7133155529afc6c3c9a08b2903
cboujoukos <cboujoukos@gmail.com>
2017-11-28 18:16:07 -0500

    work on methods

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,15 @@
 class Fish
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name, :owner
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+
+  end
+
+  def owner=(owner)
+    @owner = owner
+    
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,67 @@
+require 'pry'
+
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+    #binding.pry
+    #fish.owner = self unless fish.owner == self
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+
+  def walk_dogs
+    self.pets[:dogs].each{|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    self.pets[:cats].each{|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    self.pets[:fishes].each{|fish| fish.mood = "happy"}
+  end
+
+  def sell_pets
+    self.pets.each do |animal|
+      animal.each do |pet|
+        pet.mood = "nervous"
+      end
+    end
+  end
 end
