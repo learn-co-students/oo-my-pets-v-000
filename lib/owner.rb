fbf8e38b3cb75f950063f6bcd2b4998c531ad478
Charity <Youngblood.charity@gmail.com>
2017-12-01 15:55:39 -0800

    added placeholders for species method

--- lib/owner.rb
+++ lib/owner.rb
@@ -12,6 +12,11 @@ class Owner
   attr_accessor :pets
   attr_reader :name
   @@owners = []
+# create class methods for 
+# - all 
+# - owner_count
+# - reset_all
+
   def initialize(name)
     @pets = {fishes: [], cats: [], dogs: []}
     @@owners << self 
@@ -21,7 +26,23 @@ class Owner
     @@owners
   end 
   
+  def self.count
+    @@owners.count
+  end 
+  
   def self.reset_all 
+    # count how many elements in the @@owners array
+   # binding.pry
+    @@owners = []
+  end
+  
+  # create instance methods for 
+    # - species 
+    # - say_species 
+    # - name 
+    # - pets 
+  def species 
     
   end 
+  
 end
