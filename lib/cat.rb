b3d5d0226048810d9329c4c1251834e58236af67
Charity <Youngblood.charity@gmail.com>
2017-12-01 13:11:54 -0800

    added placheholder for initialize method

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
+# create initialize method w/name as argument 
+
+# initialize with a mood of nervous 
+  # the mood can change
 class Cat
   # code goes here
+  def initialize(name, mood)
+  
+  end 
 end
