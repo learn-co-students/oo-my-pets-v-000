8fac14cec65bae49b30b67aa7b5f951b5224c286
Charity <Youngblood.charity@gmail.com>
2017-12-01 14:23:37 -0800

    added comments on how to structure code

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,15 @@
+require 'pry'
+
+# create initialize method w/name argument 
+
+# create class methods for 
+  # - all 
+  # - owner_count
+  # - reset_all
+
 class Owner
   # code goes here
+  def initialize(name)
+  
+  end 
 end
