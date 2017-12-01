18f3b2acfb351900d0570f262f46ca54ff06069f
Charity <Youngblood.charity@gmail.com>
2017-12-01 13:38:42 -0800

    changed mood to attr_accessor; added to initialize method

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,15 +1,15 @@
 require 'pry'
 
 # create initialize method w/name as argument 
-
 # initialize with a mood of nervous 
   # the mood can change
 class Cat
   # code goes here
-  attr_reader :name
+  attr_reader :name, :mood
   attr_accessor :mood
   def initialize(name)
     @name = name 
-    @mood
+    @mood = "nervous"
   end 
+  
 end
