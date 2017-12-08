7ba178fdd1971402a8c523dc9f969505c6015e88
Nour Kodeih <nour.kodeih@gmail.com>
2017-12-08 19:41:53 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,81 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  @@all = []
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+    @@all<<self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(fish_name)
+    new_fish = Fish.new(fish_name)
+    @pets[:fishes]<<new_fish
+  end
+
+  def buy_cat(cat_name)
+    new_cat = Cat.new(cat_name)
+    @pets[:cats]<<new_cat
+  end
+
+  def buy_dog(dog_name)
+    new_dog = Dog.new(dog_name)
+    @pets[:dogs]<<new_dog
+  end
+
+  def walk_dogs
+    @pets[:dogs].map do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    @pets[:cats].map do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].map do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets[:fishes].map do |fish|
+      fish.mood = "nervous"
+    end
+    @pets[:cats].map do |cat|
+      cat.mood = "nervous"
+    end
+    @pets[:dogs].map do |dog|
+      dog.mood = "nervous"
+    end
+    @pets[:fishes].clear
+    @pets[:dogs].clear
+    @pets[:cats].clear
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
+
+
 end
