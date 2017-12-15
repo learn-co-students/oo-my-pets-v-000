c6b351548e76122a3a9e5503abbc3fa69ae8edc2
github username <github email address>
2017-12-15 18:23:59 -0500

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -4,7 +4,7 @@ class Owner
 
   @@all = []
 
-  def initialize(name)
+  def initialize(species)
     @pets = {fishes: [], cats: [], dogs: []}
     @species = species
     @@all << self
@@ -20,5 +20,52 @@ class Owner
 
   def self.count
     self.all.count
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    pets[:dogs].map do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].map do |cat|
+    cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].map do |fish|
+    fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    @pets.each do |species, pets|
+      pets.collect do |sold_pet|
+        sold_pet.mood = "nervous"
+      end
+    end
+      @pets = []
+  end
+
+  def list_pets
+     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end
 end
