60adcdba7d1e22b349bb1aa799c75e20a23026e9
funemployeddad <funemployeddad@gmail.com>
2018-01-08 11:19:18 -0700

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Owner
   # code goes here
 attr_accessor :name, :pets
@@ -7,6 +9,7 @@ attr_reader :species
   def initialize(species)
     @species = species
     @@all << self
+    @pets = {fishes: [], cats: [], dogs: []}
   end
 
   def self.all
@@ -27,34 +30,38 @@ attr_reader :species
     @species
   end
 
-  def pets
-    self.pets = {fishes: [], cats: [], dogs: []}
-  end
-
-#STILL BROKEN FROM HERE DOWN
   def say_species
-    puts "I am a #{self.species}."
+    return "I am a #{self.species}."
   end
 
   def buy_fish(name)
-    fish = Fish.new(name)
-    self.pets[:fishes] << fish
+    self.pets[:fishes] << Fish.new(name)
   end
 
-  def buy_dog
+  def buy_dog(name)
+    self.pets[:dogs] << Dog.new(name)
   end
 
-  def buy_cat
+  def buy_cat(name)
+    self.pets[:cats] << Cat.new(name)
   end
 
   def walk_dogs
-    self.pets[:dogs].mood = "happy"
+    self.pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
   end
 
   def play_with_cats
+    self.pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
   end
 
   def feed_fish
+    self.pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
   end
 
   def sell_pets
