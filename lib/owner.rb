4a86db8a2534c72e41daf85802f3b0e6236c9257
funemployeddad <funemployeddad@gmail.com>
2018-01-08 11:24:37 -0700

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -65,12 +65,21 @@ attr_reader :species
   end
 
   def sell_pets
-    self.pets.all.clear
+    self.pets[:fishes].each do |fish|
+      fish.mood = "nervous"
+    end
+    self.pets[:cats].each do |cat|
+      cat.mood = "nervous"
+    end
+    self.pets[:dogs].each do |dog|
+      dog.mood = "nervous"
+    end
 
+    self.pets.clear
   end
 
   def list_pets
-    puts "I have #{self.pets.fish.count} fish, #{self.pets.dogs.count} dog(s), and #{self.pets.cats.count} cat(s)."
+    return "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
   end
 
 end
