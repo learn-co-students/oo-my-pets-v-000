6dc3f8cb937bf2841ed1ef24574aadd1c4da5d9d
kostanzhoglo <jeffreywithers@gmail.com>
2018-01-11 14:34:08 -0500

    Add Cats, Dogs, Fishes, and Owner classes

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,79 @@
+require "pry"
+
 class Owner
   # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @@all << self
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
+    # I could also write @pets[:fishes] above and it won't break the program.  @pets is still the value I'm writing.  BUT, pets is the READER METHOD I'm calling, and that reader method will RETURN @pets.  THIS STUMPS YOU ALL THE TIME.  REMEMBER THIS JEFFREY.
+  end
+
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+
+  def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    pets[:fishes]. each do |fish|
+      fish.mood = "nervous"
+    end
+    pets[:dogs]. each do |dog|
+      dog.mood = "nervous"
+    end
+    pets[:cats]. each do |cat|
+      cat.mood = "nervous"
+    end
+    pets.clear
+  end
+
+  def list_pets
+    "I have #{pets[:fishes].length} fish, #{pets[:dogs].length} dog(s), and #{pets[:cats].length} cat(s)."
+  end
+
 end
