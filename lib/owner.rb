4d2515c77308ffab488d33dc235c3b4019eb91cf
kim alban <kimalbans@yahoo.com>
2018-01-16 03:12:24 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -72,6 +72,6 @@ end
 end
 
 def list_pets
-  "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cats(s)."
+  "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
  end
 end
