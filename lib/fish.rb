31a4afb4dea91a32d5ee71f16ae2fa39f45608d8
Sam Kalum <stkalum@gmail.com>
2018-01-17 14:25:42 -0500

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,16 @@
 class Fish
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  def initialize(name)
+    self.name = name
+    self.mood = "nervous"
+  end
+
+  private
+
+  def name=(name)
+    @name = name
+  end
+
 end
