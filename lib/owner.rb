eecd3f4201d117ba0ab3446ef51bdc4ee069bafd
Judy McGoldrick <jjeanius@verizon.net>
2018-01-22 04:51:16 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,75 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all =[]
+
+  def initialize(species)
+    @pets = {:fishes=>[], :dogs=>[], :cats=>[]}
+    @species = species
+    @name = name
+    @@all << self
+
+  end
+
+  def species
+    @species
+
+  end
+
+   def say_species
+     "I am a #{species}."
+   end
+
+   def buy_fish(name)
+     fish = Fish.new(name)
+     pets[:fishes] << fish
+   end
+
+   def buy_dog(name)
+     dog = Dog.new(name)
+     pets[:dogs] << dog
+   end
+
+   def buy_cat(name)
+     cat = Cat.new(name)
+     pets[:cats] << cat
+   end
+
+   def walk_dogs
+     pets[:dogs].map {|dog| dog.mood = "happy"}
+ end
+
+   def play_with_cats
+     pets[:cats].map {|cat| cat.mood = "happy"}
+   end
+
+   def feed_fish
+     pets[:fishes].map {|fish| fish.mood = "happy"}
+   end
+
+   def sell_pets
+     pets.each do|type, pets|
+       pets.map{|pet| pet.mood = "nervous"}
+     end
+     pets.clear
+   end
+
+   def list_pets
+     "I have #{pets[:fishes].size} fish, #{pets[:dogs].size} dog(s), and #{pets[:cats].size} cat(s)."
+   end
+
+   def self.all
+     @@all
+   end
+
+   def self.reset_all
+     @@all.clear
+   end
+
+   def self.count
+     @@all.count
+   end
+
 end
