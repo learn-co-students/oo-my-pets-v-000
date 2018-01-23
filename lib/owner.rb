e3e5fbed0fc0487d57cf7886d49251c8bb58e2ee
Aubrey Neimeier <neimeier.1@osu.edu>
2018-01-23 19:49:05 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -8,6 +8,7 @@ class Owner
     @species = species
     @@all << self
     @pets = {:fishes => [], :dogs => [], :cats => []}
+    #values are stored in arrays, therefore we will shovel values in
   end
 
   def self.all
@@ -33,17 +34,20 @@ class Owner
   def buy_fish(name)
     fish = Fish.new(name)
     @pets[:fishes] << fish
+    #could be one line as
+    #@pets[:fishes]<<Fish.new(name)
   end
 
   def buy_cat(name)
     cat = Cat.new(name)
     @pets[:cats] << cat
+    #@pets[:cats] << Cat.new(name)
   end
 
   def buy_dog(name)
     dog = Dog.new(name)
     @pets[:dogs] << dog
-
+    #@pets[:dogs] << Dog.new(name)
   end
 
   def walk_dogs
@@ -60,15 +64,35 @@ class Owner
     @pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
-  def sell_pets()
+  def sell_pets
+    pets.each do |species, pets|
+      pets.each do |pet|
+        pet.mood = "nervous"
+      end
+      pets.clear
+    end
+  end
+
+=begin
+///// literal version of keys and values
+    def sell_pets
+      pets.each do |keys, values|
+        values.each do |value|
+          value.mood = "nervous"
+        end
+        values.clear
+      end
+    end
+
+/////hard-coding the answer//// instead, iterate like above
     @pets[:fishes].each {|x| x.mood = "nervous"}
     @pets[:cats].each {|x| x.mood = "nervous"}
     @pets[:dogs].each {|x| x.mood = "nervous"}
     @pets[:fishes].clear
     @pets[:cats].clear
     @pets[:dogs].clear
+=end
 
-  end
 
   def list_pets
     "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
