067fd03b9f02fef9d47c09986957222c55d97626
Alli Tzadok <allisontzadok@gmail.com>
2018-01-23 20:41:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,13 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+
+  #can initialize a Cat with a name
+  #name cannot be changed. - use a attr_reader
+  #initialize with a nervous mood
+  #mood can be changed - attr_accessor
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
 end
