3fe8c51eb16cbe9da63339a492cf5ae27957e7cd
Connie Hyman <constance.hyman@mchsi.com>
2018-01-23 20:58:16 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,15 +1,15 @@
 class Owner
   # code goes here
   attr_reader :species
-  attr_accessor :name, :pets, :fish, :dog, :cat
+  attr_accessor :name, :pets
 
 
   @@all = []
   #@count = 0
 
-  def initialize(name)
-    @name = name
-    @species = "human"
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
     @@all << self
   end
 
@@ -25,51 +25,58 @@ class Owner
     @@all.count
   end
 
-  def species
-    @species
-  end
 
   def say_species
-    "I am a human."
-  end
-
-  def name=(name)
-    @name = name
-  end
-
-  def pets
-    pets = {:fishes => [], :dogs => [], :cats => []}
+    "I am a #{species}."
   end
 
 
   def buy_fish(name)
-    fish = Fish.new(name)
-    
+     fish = Fish.new(name)
+     pets[:fishes] << fish
+   end
 
+
+  def buy_cat(name)
+    new_cat = Cat.new(name)
+    pets[:cats] << new_cat
   end
 
-  def buy_cat
-
-
-
-  end
-
-  def buy_dog
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    pets[:dogs] << new_dog
   end
 
   def walk_dogs
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
   end
 
   def play_with_cats
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
   end
 
   def feed_fish
+    pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
   end
 
   def sell_pets
+    pets.each do |species, animals|
+      animals.each do |animal|
+        animal.mood = "nervous"
+      end
+      animals.clear
+    end
   end
 
   def list_pets
+       "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+
   end
 
 end
