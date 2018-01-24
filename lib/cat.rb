5732fce6d00ffb4baeb7df5fdaf0721b0d8fab4f
Ryne Stajcar <rynostajcar@gmail.com>
2018-01-24 03:11:36 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,15 @@
 class Cat
-  # code goes here
+
+  attr_accessor :mood
+  attr_reader :name
+      @@all = []
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
