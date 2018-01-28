b00be03c0ac10e0e5ccddf3ea9ba80995befa381
Barbara Choy <barbchoy@gmail.com>
2018-01-28 01:46:34 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,4 +43,33 @@ class Owner
       dog.mood = "happy"
     end
   end
+
+  def play_with_cats
+    @pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    @pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end
+
+  def sell_pets
+    @pets[:dogs].each do |dog|
+      dog.mood = "nervous"
+    end
+    @pets[:cats].each do |cat|
+      cat.mood = "nervous"
+    end
+    @pets[:fishes].each do |fish|
+      fish.mood = "nervous"
+    end
+    @pets = {}
+  end
 end
