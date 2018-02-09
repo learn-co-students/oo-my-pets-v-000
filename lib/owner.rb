cc73fdf962a9dbefc410a86ebd9031bdcccd0b1a
Chaim Reisman <crreisman@gmail.com>
2018-02-09 03:53:05 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,25 @@
+require'pry'
 class Owner
   # code goes here
+  attr_reader :name
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end 
+    
+  def self.all
+    @@all 
+  end 
+  
+  def self.count
+    #binding.pry
+    @@all.count
+  end
+  
+  def self.reset_all
+    
+  end 
+  
 end
