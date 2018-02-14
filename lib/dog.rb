45b95d5a1c8e3bcf5de6387a5caa119b951d76b3
Eden Mesfun <emesfun@gmail.com>
2018-02-14 18:04:11 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,22 @@
 class Dog
-  # code goes here
+  attr_accessor :mood
+
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+
+
+
+  # def walked_dog
+  #   if Owner.walk_dogs
+  #     @mood = "happy"
+  #   else
+  #     @mood = "nervous"
+  #   end
+  # end
+
 end
