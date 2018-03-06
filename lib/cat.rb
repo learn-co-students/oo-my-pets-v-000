b519638cedc51f511edabcb4db155c870cf62c31
Nathan Dietz <nsdietz@gmail.com>
2018-03-06 12:55:22 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_accessor :name, :mood
+
+  def initialize(name, mood = "nervous")
+    @name = name.freeze
+    @mood = mood
+  end
+
+  def name=(name)
+    raise NoMethodError if @name != name
+  end
+
 end
