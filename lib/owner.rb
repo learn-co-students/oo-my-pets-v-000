1a375305cc7277a904fe711f83644c6d6f5a84c4
Dee Race <deerace12@gmail.com>
2018-03-14 19:54:54 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -27,7 +27,7 @@ class Owner
   end
 
   def say_species
-    return "I am a #{@species}."
+    "I am a #{species}."
   end
 
   #Pets
@@ -45,48 +45,33 @@ class Owner
   end
 
   def walk_dogs
-    @pets.collect do |species, instances|
-      if species == :dogs
-        instances.each do |dog|
-          dog.mood = "happy"
-        end
-      end
+    pets[:dogs].each do |dog|
+      dog.mood = "happy"
     end
   end
 
   def play_with_cats
-    @pets.collect do |species, instances|
-      if species == :cats
-        instances.each do |cat|
-          cat.mood = "happy"
-        end
-      end
+    pets[:cats].each do |cat|
+      cat.mood = "happy"
     end
   end
 
   def feed_fish
-    @pets.collect do |species, instances|
-      if species == :fishes
-        instances.each do |fish|
-          fish.mood = "happy"
-        end
-      end
-    end
+     pets[:fishes].each do |fish|
+       fish.mood = "happy"
+     end
   end
 
   def sell_pets
-    @pets.collect do |species, instances|
-      instances.each do |pet|
+    @pets.each do |species, animals|
+      animals.each do |pet|
         pet.mood = "nervous"
       end
-      instances.clear
+      animals.clear
     end
   end
 
   def list_pets
-    num_dogs = @pets[:dogs].size
-    num_cats = @pets[:cats].size
-    num_fish = @pets[:fishes].size
-    return "I have #{num_fish} fish, #{num_dogs} dog(s), and #{num_cats} cat(s)."
+    "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
   end
 end
