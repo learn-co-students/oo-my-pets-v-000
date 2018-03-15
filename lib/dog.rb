128f1fa27e3b49c4134af1ef8a1e09ed9477e912
Harleigh Abel <harleighabel@gmail.com>
2018-03-15 00:18:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 class Dog
   attr_reader :name, :species
   attr_accessor :mood
+  
   def initialize(name, mood= "nervous")
     @name = name
     @mood = mood
     @species = "dog"
   end  
-    
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,14 +1,14 @@
+require 'pry'
 class Owner
   @@all = []
-  attr_accessor :pets
-  attr_reader :species, :name
-
+  attr_accessor :pets, :name
+  attr_reader :species, :pets
   
   def initialize(name)
-    @name = name
+    @owner = name
     @pets = {fishes: [], cats: [], dogs: []}
-    @@all << name
     @species = "human"
+    @@all << self
   end  
   
   def self.all
@@ -22,7 +22,6 @@ class Owner
   def self.count
     @@all.count
   end  
-    
   
   def species_initialize(species)
     if species == "cat"
@@ -38,39 +37,45 @@ class Owner
     "I am a #{@species}."
   end
   
-  def name
-    
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
   end
   
-  def pets 
-    @pets
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
   end
   
-  def buy_fish
-    
-  end
-  
-  def buy_cat
-    
-  end
-  
-  def buy_dog
-    
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
   end
   
   def walk_dogs
-    
+    @pets[:dogs].map {|dog| dog.mood = "happy"}
   end
   
   def play_with_cats
-    
+    @pets[:cats].map {|cat| cat.mood = "happy"}
   end
+  
+  def feed_fish
+      @pets[:fishes].map {|fish| fish.mood = "happy"}
+  end  
+ 
   
   def sell_pets
     
+    @pets.each do |owner_hash, pets_array| 
+      pets_array.map do |pet| 
+        pet.mood = "nervous"
+      end
+    end  
+    @pets = {fishes: [], cats: [], dogs: []}
   end
   
   def list_pets
-    
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
   end  
 end
