8573cd97e387804cc07588eb19b0a6fb36d89a02
Harleigh Abel <harleighabel@gmail.com>
2018-03-15 00:20:53 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,7 +34,7 @@ class Owner
   end  
   
   def say_species
-    "I am a #{@species}."
+    "I am a #{self.species}."
   end
   
   def buy_fish(name)
@@ -44,38 +44,38 @@ class Owner
   
   def buy_cat(name)
     cat = Cat.new(name)
-    @pets[:cats] << cat
+    self.pets[:cats] << cat
   end
   
   def buy_dog(name)
     dog = Dog.new(name)
-    @pets[:dogs] << dog
+    self.pets[:dogs] << dog
   end
   
   def walk_dogs
-    @pets[:dogs].map {|dog| dog.mood = "happy"}
+    self.pets[:dogs].map {|dog| dog.mood = "happy"}
   end
   
   def play_with_cats
-    @pets[:cats].map {|cat| cat.mood = "happy"}
+    self.pets[:cats].map {|cat| cat.mood = "happy"}
   end
   
   def feed_fish
-      @pets[:fishes].map {|fish| fish.mood = "happy"}
+      self.pets[:fishes].map {|fish| fish.mood = "happy"}
   end  
  
   
   def sell_pets
     
-    @pets.each do |owner_hash, pets_array| 
+    self.pets.each do |owner_hash, pets_array| 
       pets_array.map do |pet| 
         pet.mood = "nervous"
       end
     end  
-    @pets = {fishes: [], cats: [], dogs: []}
+    self.pets = {fishes: [], cats: [], dogs: []}
   end
   
   def list_pets
-    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+    "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
   end  
 end
