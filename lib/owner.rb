96da0fbac0d022d75a91424880fb8b707499f240
Arye Dov Eidelman <aryedov.e@gmail.com>
2018-03-16 15:49:40 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,5 @@
 class Owner
-  attr_reader :species
+  attr_reader :species, :pets
   attr_accessor :name
   @@all = []
 
@@ -7,10 +7,23 @@ class Owner
     @name = name
     @@all << self
     @species = 'human'
+    @pets = {:fishes => [], :dogs => [], :cats => []}
   end
 
   def say_species
     "I am a #{@species}."
+  end
+
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+
+  def buy_cat(name)
+    @pets[:cats] << Cat.new(name)
+  end
+
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
   end
 
   def self.all
