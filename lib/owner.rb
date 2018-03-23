e7c69d16a2243052809d29ffb90f252e39b1e89c
Alwina Oyewoleturner <aoyewole2586@gmail.com>
2018-03-23 02:34:23 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,31 @@
+require "pry"
+
 class Owner
+
+  attr_accessor :name
+  attr_reader :species
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    # self.all = 0
+    self.all += 1
+  end
+
+  def self.clear_all
+    @@all =[]
+
+  end
+
+  def initialize(species)
+    @species = species
+
+    @@all << self
+
+  end
   # code goes here
 end
