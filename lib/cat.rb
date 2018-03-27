426fbefe0b57ecb5d79dbabb1a0112488eb2ca42
Neena van de Water <nkuriakose@hotmail.com>
2018-03-27 17:30:06 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
+require 'pry'
+
 class Cat
-  # code goes here
+ attr_accessor :mood
+ attr_reader :name
+
+ def initialize(name)
+  #  binding.pry
+   @name = name
+   @mood = "nervous"
+ end# code goes here
+
 end
