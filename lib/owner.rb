3c4c75e4bde5012dc6c4ff181dbc818884173a73
Aderianna T Williams <aderiannaw@yahoo.com>
2018-03-28 03:21:47 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,5 +1,5 @@
 class Owner
-  attr_accessor :owner, :pets
+  attr_accessor :name, :pets
   attr_reader :species
   @@all = []
   
@@ -21,9 +21,49 @@ class Owner
     @@all =[]
   end
   
-  def self.say_species
-    @species = species
-    
+  def say_species
+  "I am a #{species}."
   end
   
+  def buy_fish(name)
+    pets[:fishes] << Fish.new(name)
+  end
+  
+  def buy_cat(name)
+    pets[:cats] << Cat.new(name)
+  end
+  
+  def buy_dog(name)
+    pets[:dogs] << Dog.new(name)
+  end
+  
+  def walk_dogs
+    pets[:dogs].map do |dog|
+      dog.mood = "happy"
+    end
+  end
+  
+  def play_with_cats
+    pets[:cats].map do |cat|
+      cat.mood = "happy"
+    end
+  end
+  
+    def feed_fish
+    pets[:fishes].map do |fish|
+      fish.mood = "happy"
+    end
+  end
+  
+  def sell_pets
+        pets.each do |species, pets|
+      pets.each do |pet|
+        pet.mood = "nervous"
+    end
+    pets.clear
+  end
+end
+def list_pets
+   "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
+end
 end
