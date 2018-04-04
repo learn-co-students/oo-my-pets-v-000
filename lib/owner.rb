73809bab638f1763f874676b6d33f84cec2090d1
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-04 23:26:21 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,65 @@
+require "pry"
 class Owner
-  # code goes here
+  attr_reader :pets, :species, :name
+  #note on pets writer: this is needed by test.  We should really
+  #have explicit method that verifies pets hash is valid
+  #or require Owner buys pets (remove writer)
+  attr_writer :pets, :name
+
+  @@all = []
+
+  def initialize (species)
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: []}
+    @name = nil
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_cat(name)
+    cat = Cat.new(name)
+    @pets[:cats] << cat
+  end
+  def play_with_cats
+    @pets[:cats].each { | cat | cat.mood="happy"}
+  end
+  def buy_dog(name)
+    dog = Dog.new(name)
+    @pets[:dogs] << dog
+  end
+  def walk_dogs
+    @pets[:dogs].each { | dog | dog.mood="happy"}
+  end
+
+  def buy_fish(name)
+    fish = Fish.new(name)
+    @pets[:fishes] << fish
+  end
+  def feed_fish
+    @pets[:fishes].each { | fish | fish.mood="happy"}
+  end
+
+  def sell_pets
+    @pets.each do | key, val |
+      val.each { | pet | pet.mood="nervous"}
+      val.clear
+    end
+  end
+
+  def list_pets
+    "I have #{@pets[:fishes].count} fish, #{@pets[:dogs].count} dog(s), and #{@pets[:cats].count} cat(s)."
+  end
+
+  def self.all
+    @@all
+  end
+  def self.reset_all
+    @@all.clear
+  end
+  def self.count
+    @@all.count
+  end
 end
