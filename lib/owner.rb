0ce953d7301f924b784e26be38843d8b8ea37c21
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-04-04 23:29:19 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -20,23 +20,20 @@ class Owner
   end
 
   def buy_cat(name)
-    cat = Cat.new(name)
-    @pets[:cats] << cat
+    @pets[:cats] << Cat.new(name)
   end
   def play_with_cats
     @pets[:cats].each { | cat | cat.mood="happy"}
   end
   def buy_dog(name)
-    dog = Dog.new(name)
-    @pets[:dogs] << dog
+    @pets[:dogs] << Dog.new(name)
   end
   def walk_dogs
     @pets[:dogs].each { | dog | dog.mood="happy"}
   end
 
   def buy_fish(name)
-    fish = Fish.new(name)
-    @pets[:fishes] << fish
+    @pets[:fishes] << Fish.new(name)
   end
   def feed_fish
     @pets[:fishes].each { | fish | fish.mood="happy"}
