e8111f8881953ad19c176d7513fea5bf52086af6
Diego Yanez <diegosyanez92@gmail.com>
2018-04-17 17:33:19 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
+require 'pry'
 class Owner
   # code goes here
+  attr_accessor :fish, :cat, :dog, :name
+  attr_reader :species
+
+  @@ownercount = 0
+
+    def self.all
+      @@all
+    end
+
+    def self.count
+      @@count
+    end
+
+    def initialize(species)
+      @species = species
+      @all
+    end
+
+
 end
