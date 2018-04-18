559806d1149781c2c745bddd179cfd035b80bb17
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-18 23:12:12 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,23 @@
 class Owner
-  # code goes here
+  attr_accessor :pets, :species
+  @pets = {fishes: [], cats: [], dogs: []}
+  @@all = []
+  
+  def initialize(pets)
+    @pets = pets
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    self.all.count
+  end
+  
+  def self.reset_all
+    
+  end
+  
 end
