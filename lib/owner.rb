00699b56851f687897ef7944bbe978528458e8a4
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-19 21:20:33 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,16 +28,38 @@ class Owner
   end
   
   def buy_fish(name)
-    
-    self.pets.first.name
+    new_fish = Fish.new(name)
+    self.pets[:fishes] << new_fish
   end
   
   def buy_cat(name)
-    
+    new_cat = Cat.new(name)
+    self.pets[:cats] << new_cat
   end
   
   def buy_dog(name)
+    new_dog = Dog.new(name)
+    self.pets[:dogs] << new_dog
+  end
+  
+  def walk_dogs
+    @pets[:dogs].mood="happy"
+  end
+  
+  def play_with_cats
     
+  end
+  
+  def feed_fish
+    
+  end
+  
+  def sell_pets
+    
+  end
+  
+  def list_pets
+    "I have 2 fish, 3 dog(s), and 1 cat(s)."
   end
   
 end
