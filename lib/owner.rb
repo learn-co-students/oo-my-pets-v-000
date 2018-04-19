d7a64d345e395ec71412ad0eaf78378a7e266a12
Valerie Lynie McClimon <vmcclimon33@yahoo.com>
2018-04-19 22:16:44 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,23 +43,34 @@ class Owner
   end
   
   def walk_dogs
-    @pets[:dogs].mood="happy"
+    @pets[:dogs].each do |dog|  
+      dog.mood="happy"
+    end
   end
   
   def play_with_cats
-    
+    @pets[:cats].each do |cat|  
+      cat.mood="happy"
+    end
   end
   
   def feed_fish
-    
+    @pets[:fishes].each do |fish|  
+      fish.mood="happy"
+    end
   end
   
   def sell_pets
-    
+    @pets.each do |key, values|
+      values.each do |pet|
+        pet.mood="nervous"
+      end
+      @pets = {}
+    end
   end
   
   def list_pets
-    "I have 2 fish, 3 dog(s), and 1 cat(s)."
+    "I have #{@pets[:fishes].length} fish, #{@pets[:dogs].length} dog(s), and #{@pets[:cats].length} cat(s)."
   end
   
 end
