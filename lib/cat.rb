13ac8ea67c19944ff0070b05c59e6ff6af4277ae
Pamela Frieze <pfrieze@gmail.com>
2018-05-01 19:53:58 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,11 @@
 class Cat
   # code goes here
+  attr_reader :name #can only read the attribute
+  attr_accessor :mood #reads and sets the attribute
+
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
 end
