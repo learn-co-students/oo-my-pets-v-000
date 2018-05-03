64e8f95af0f15104ea4da0490dac497e63672409
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-03 17:55:26 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,26 @@
 class Owner
-  # code goes here
+
+  attr_accessor :name, :pets, :say_species, :buy_fish, :buy_cat, :buy_dog, :walk_dogs, :play_with_cats, :feed_fish, :sell_pets, :list_pets
+  attr_reader :species
+
+@@all = []
+
+  def initialize(name)
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+    @species = "human"
+  end
+
+def self.all
+  @@all
+end
+
+def save
+  @@all << self
+end
+
+def reset_all
+  @@all.clear
+end
+
 end
