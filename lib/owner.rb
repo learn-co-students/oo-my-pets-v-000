dc1dc70ce2bc98b5a1b95e9eddb2830823d8016a
Adam Kalt <adam.kalt.1@gmail.com>
2018-05-03 18:42:51 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,13 +1,15 @@
 class Owner
 
-  attr_accessor :name, :pets, :say_species, :buy_fish, :buy_cat, :buy_dog, :walk_dogs, :play_with_cats, :feed_fish, :sell_pets, :list_pets
+  attr_accessor :name, :pets, :say_species, :buy_fish, :buy_cat, :buy_dog,
+  :walk_dogs, :play_with_cats, :feed_fish, :sell_pets, :list_pets
   attr_reader :species
 
 @@all = []
 
   def initialize(name)
-    @name = name
+    @name = "human"
     @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
     @species = "human"
   end
 
@@ -15,12 +17,41 @@ def self.all
   @@all
 end
 
-def save
-  @@all << self
+def self.reset_all
+  @@all.clear
 end
 
-def reset_all
-  @@all.clear
+def self.count
+  @@all.length
+end
+
+def say_species
+"I am a human."
+end
+
+def buy_fish(name)
+  @pets[:fishes] = Fish.new(name)
+end
+
+def buy_cat(name)
+end
+
+def buy_dog(name)
+end
+
+def walk_dogs
+end
+
+def play_with_cats
+end
+
+def feed_fish
+end
+
+def sell_pets
+end
+
+def list_pets
 end
 
 end
