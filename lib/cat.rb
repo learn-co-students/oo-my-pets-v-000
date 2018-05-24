96a42b063cc00457b17ac593a8378bc61cbef777
Brennan Fulmer <brennan.flmr@gmail.com>
2018-05-24 19:23:32 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,19 @@
+
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+  
+  def name=(name)
+    if @name == nil
+      @name = name
+    else
+      raise NoMethodError, "Species is already set as #{self.name}"
+    end
+  end
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,43 @@
+
 class Owner
-  # code goes here
+  attr_accessor :name
+  attr_reader :pets, :species
+  @@owner = []
+  
+  def initialize(species)
+    @pets = { fishes: [], cats: [], dogs: [] }
+    self.species = species
+    @@owner << self
+  end
+  
+  def self.all
+    @@owner
+  end
+  
+  def self.reset_all
+    self.all.clear
+  end
+  
+  def self.count
+    self.all.size
+  end
+  
+  def species=(species)
+    if @species == nil
+      @species = species
+    else
+      raise NoMethodError, "Species is already set as #{self.species}"
+    end
+  end
+  
+  def say_species
+    "I am a #{self.species}."
+  end
+  
+  # def buy_fish
+  
 end
+
+
+
+# spacing
