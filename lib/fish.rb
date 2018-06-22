b782ada24ed1353bbfdcc00736ceab5eb02abba3
Kevin Chi <kevinchi118@gmail.com>
2018-06-22 16:15:58 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,16 @@
 class Fish
-  # code goes here
+    attr_accessor :mood
+    attr_reader :name
+
+
+
+    def initialize(name)
+        @name = name
+        @mood = "nervous"
+    end
+
+    def species
+        "fish"
+    end
+
 end
