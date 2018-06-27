a21baf6db2a62523e07bced5ee14522628073d8e
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-27 19:47:13 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,13 @@
 class Cat
-  # code goes here
+  attr_accessor :mood
+  attr_reader :name 
+  def initialize(name)
+    @name = name 
+    @mood = "nervous"
+  end
+  
+  def buy_cat(name)
+    new_cat = Cat.new 
+    new_cat.name = name 
+  end 
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-  # code goes here
+    attr_accessor :mood
+    attr_reader :name
+    
+  def initialize(name)
+    @name = name 
+    @mood = "nervous"
+  end 
+
+  def buy_dog(name)
+    new_dog = Dog.new 
+    new_dog.name = name 
+  end 
 end
