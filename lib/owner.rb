dc9cc3d7d9026e29fb45f63eebbff88a297d89bd
jeremy.zinger@gmail.com <jeremy.zinger@gmail.com>
2018-07-10 14:02:02 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -28,27 +28,46 @@ class Owner
   end
   
   def walk_dogs
-    @pets[:dogs].each do {| mood | dog.mood = "happy"}
+    @pets[:dogs].each do | mood | 
+      dog.mood = "happy"
+    end
   end
   
   def play_with_cats
-    @pets[:cats].each do {| mood | cat.mood = "happy"}
+    @pets[:cats].each do | mood | 
+      cat.mood = "happy"
+    end
   end
   
   def feed_fish
-    @pets[:fish].each do {| mood | fish.mood = "happy"}
+    @pets[:fish].each do | mood | 
+      fish.mood = "happy"
+    end
   end
   
   def sell_pets
-    @pets.each do {| type, name_array | name.array.each do | pet | pet.mood = "nervous"}
+    @pets.each do | type, name_array | 
+      name.array.each do | pet | 
+        pet.mood = "nervous"
+      end
+    end
     @pets = {}
   end
       
+  def list_pets
+    "I have #{@pets[:fish].length} fish, #{@pets[:dogs.length]} dog(s), and #{@pets[:cats].length} cat(s)."
+  end
   
-#  def buy_cat/dog/fish(name)
-#    @name = name
-#    @pets << name
-#  end
-
+  def self.all
+    @@owners
+  end
+  
+  def self.reset.all
+    @@owners.clear
+  end
+  
+  def self.count
+    @@owners.length
+  end
 
 end
