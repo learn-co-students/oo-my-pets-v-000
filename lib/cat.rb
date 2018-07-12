58b78ac223fc24ff5797ccd67a85da8e84657b23
Jordan Harris <jharris@wework.com>
2018-07-12 22:41:47 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,9 @@
 class Cat
-  # code goes here
+  attr_accessor :name, :mood 
+  
+  def intialize(name)
+    @name = name
+    @mood = 'nervous'
+  end
+
 end
--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,9 @@
 class Dog
-  # code goes here
+  attr_accessor :name, :mood
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
 end
