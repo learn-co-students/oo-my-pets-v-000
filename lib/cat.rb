1f95e0bbae5b19d7a40c04dfb985efa6dc026954
Valencia Caldwell <vkcaldwell@gmail.com>
2018-07-25 15:55:18 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,16 @@
 class Cat
+   attr_accessor :owner, :mood
+   attr_reader :name
+  
+  def initialize(name, mood= "nervous")
+   @name = name 
+   @mood= mood
+   
+  end
+  
+  def self.mood
+    @mood
+  end
+  
   # code goes here
 end
