15a3223dc60b501ff06846d16c08376082d4d769
Azhar Shah <azharshahstl@yahoo.com>
2018-08-08 16:10:58 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
 class Owner
-  # code goes here
+  @@all = []
+  attr_accessor :owner_name
+  attr_reader :species
+  
+  def initialize(owner_name)
+    owner = Owner.new(owner_name)
+    @owner_name = owner_name
+  end
+  
+  def self.all
+    @@all << self 
+  end
+  
+  def self.reset_all 
+    count = @@all.size
+  end 
+  
+  def self.count 
+
+  end
+  
+  def species
+    @species = species
+  end 
+  
 end
