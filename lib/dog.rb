ceeffdaf2a0df410254e640f4d10384e9391fe6b
Daniel Etter <etter.dt@gmail.com>
2018-08-09 17:27:31 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,17 @@
 class Dog
-  # code goes here
+
+  attr_accessor :mood, :owner
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+  
+  # def new_dog(name)
+  #   my_new_dog = self.new(name)
+  #   my_new_dog.owner_name = @owner
+  #   my_new_dog
+  # end
+  
 end
