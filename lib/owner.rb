81c997e050892911b030da84dc52f50e5d436979
Azhar Shah <azharshahstl@yahoo.com>
2018-08-09 17:29:59 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -34,4 +34,35 @@ class Owner
     pets[:fishes] << fish
   end
   
+  def buy_dog(name)
+    fish = Dog.new(name) 
+    pets[:dogs] << fish
+  end
+  
+  def buy_cat(name)
+    fish = Cat.new(name) 
+    pets[:cats] << fish
+  end
+  
+  def walk_dogs
+    #binding.pry
+    dog = self.pets[:dogs]
+    dog.mood = "happy"
+    pets[:dogs]
+  end
+  
+  def play_with_cats 
+    
+  end 
+  
+  def feed_fish 
+    
+  end
+  
+  def list_pets 
+    number_of_fish = pets[:fishes].size 
+    number_of_dogs = pets[:dogs].size
+    number_of_cats = pets[:cats].size 
+    return "I have #{number_of_fish} fish, #{number_of_dogs} dog(s), and #{number_of_cats} cat(s)."
+  end
 end
