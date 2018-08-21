7f1923cb3fe1d64631dbe59320082d56b64e81d4
jamieberrier <berrier.jamie@gmail.com>
2018-08-21 17:16:20 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,20 @@
+require 'pry'
+
 class Dog
   # code goes here
+  attr_reader :name
+  
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+
+  def mood=(mood)
+    @mood = mood
+  end
+
+  def mood
+    @mood
+  end
+
 end
