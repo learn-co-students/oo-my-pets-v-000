3c2e14bde3e5adb9c0dd679fa48052e1e106a06b
Heshie Brody <4hbrody@gmail.com>
2018-08-29 18:30:24 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,47 @@
 class Owner
-  # code goes here
+  @@all = [ ]
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @pets = {:cats => [ ], :dogs => [ ], :fishes => [ ]}
+    @@all << self
+  end
+# Instance methods
+  def say_species
+    "I am a #{@species}."
+  end
+  def buy_fish(name)
+    @pets[:fishes] << Fish.new(name)
+  end
+  def buy_cat(name)
+     @pets[:cats] << Cat.new(name)
+  end
+  def buy_dog(name)
+    @pets[:dogs] << Dog.new(name)
+  end
+  def walk_dogs
+    pets[:dogs].each {|d| d.mood = "happy"}
+  end
+  def play_with_cats
+    pets[:cats].each {|c| c.mood = "happy"}
+  end
+  def feed_fish
+    pets[:fishes].each {|f| f.mood = "happy"}
+  end
+  def sell_pets
+
+  end
+# Class methods
+  def self.all
+    @@all
+  end
+  def self.count
+    self.all.count
+  end
+  def self.reset_all
+    self.all.clear
+  end
 end
