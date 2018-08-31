355b0c59fb831e5e95d70a6d8c525fb9285d6ed2
Nadia Fernandez Alonso <nadiafa581@gmail.com>
2018-08-31 23:41:59 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,12 @@
 class Fish
-  # code goes here
+  attr_accessor  :owner, :mood
+  attr_reader :name
+  @@all =[]
+  
+  def initialize(name, mood = "nervous")
+    @name = name 
+    @mood = mood
+    @@all << self
+  end 
+    
 end
