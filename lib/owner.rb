a610ea71d561648d953e573ea3445ea6f4757bf5
Steven Crouse <sdcrouse91@gmail.com>
2018-09-07 09:02:41 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,66 @@
+require 'pry'
 class Owner
   # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+  
+  @@all = []
+  
+  def initialize(species)
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+  end
+  
+  def self.all 
+    @@all
+  end 
+  
+  def self.reset_all 
+    @@all.clear
+  end
+  
+  def self.count
+    self.all.count
+  end 
+  
+  def say_species 
+    "I am a #{self.species}."
+  end
+  
+  def buy_fish(fish_name)
+    self.pets[:fishes] << Fish.new(fish_name)
+  end
+  
+  def buy_cat(cat_name)
+    self.pets[:cats] << Cat.new(cat_name)
+  end
+  
+  def buy_dog(dog_name)
+    self.pets[:dogs] << Dog.new(dog_name)
+  end
+  
+  def walk_dogs 
+    self.pets[:dogs].each {|dog| dog.mood = "happy"}
+  end 
+  
+  def play_with_cats 
+    self.pets[:cats].each {|cat| cat.mood = "happy"}
+  end 
+  
+  def feed_fish
+    self.pets[:fishes].each {|fish| fish.mood = "happy"}
+  end 
+  
+  def sell_pets 
+    self.pets.each do |pet_type, pet_array| 
+      pet_array.each {|pet| pet.mood = "nervous"}
+      pet_array.clear
+    end
+    #binding.pry
+  end
+  
+  def list_pets
+    "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
+  end
 end
