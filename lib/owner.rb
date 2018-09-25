adc532bd263b1e85668b692f50731c320b470ebb
Ashley Wong <ashwong04@gmail.com>
2018-09-25 12:08:58 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,27 @@
 class Owner
-  # code goes here
+  
+  attr_accessor :name
+  
+  @@all = []
+  @@count = 0
+  
+  def initialize(name)
+    @name= name
+    @@all << self
+    @@count += 1
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def self.count
+    @@count
+  end
+  
+  def self.reset_all
+    @@count = 0
+  end
+  
+  
 end
