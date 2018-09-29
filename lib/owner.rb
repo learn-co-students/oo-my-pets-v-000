40272e005fa1bf9f95ee3e43006b0b4048bcd0f1
John Werenski <jtwerenski@gmail.com>
2018-09-29 14:16:44 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,84 @@
+require 'pry'
+
 class Owner
-  # code goes here
-end
+  attr_accessor :name, :pets
+  attr_reader :species 
+
+  @@owners = []
+
+  def initialize(species)
+    @species = species
+    @pets = {
+      fishes: [],
+      dogs: [],
+      cats: []
+    }
+    @@owners << self
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.count
+    @@owners.count
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+
+  def say_species
+    "I am a #{self.species}."
+  end
+
+
+  def buy_fish(fish_name)
+    new_fish = Fish.new(fish_name)
+    self.pets[:fishes] << new_fish
+  end
+
+  def buy_cat(cat_name)
+    new_cat = Cat.new(cat_name)
+    self.pets[:cats] << new_cat
+  end
+
+  def buy_dog(name)
+    new_dog = Dog.new(name)
+    self.pets[:dogs] << new_dog
+  end
+
+  def walk_dogs
+    self.pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+  def play_with_cats
+    self.pets[:cats].each do |cat|
+      cat.mood = "happy"
+    end
+  end
+
+  def feed_fish
+    self.pets[:fishes].each do |fish|
+      fish.mood = "happy"
+    end
+  end
+
+  def sell_pets
+    #binding.pry
+    self.pets.each do |key, array|
+      array.each {|pet| pet.mood = "nervous"}
+    end
+    #binding.pry
+    self.pets.update(self.pets) { |key, value| value = [] }
+  end
+
+  
+  def list_pets
+   "I have #{self.pets[:fishes].length} fish, #{self.pets[:dogs].length} dog(s), and #{self.pets[:cats].length} cat(s)."
+  end 
+
+end 
