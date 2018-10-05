62473e218f525b46b0ef4247c961d43172f72dff
Cheryl K <cherylkelson@icloud.com>
2018-10-05 16:59:05 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
+#require 'pry'
 class Cat
-  # code goes here
+  
+  attr_accessor :name, :mood 
+  
+
+  def initialize(name = :name)
+    @name = name 
+    @mood = "nervous"
+   
+   #
+   binding.pry 
+  end 
+
 end
+
+# def initialize(:name)   Using symbol name = :name because symbols are immutable
+
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,34 @@
 class Owner
-  # code goes here
-end
+  
+    OWNERS = [] 
+    
+    attr_accessor :name, :pets, :species 
+
+
+  def initialize
+  OWNERS << SELF
+  @pets = {fishes:[], cats:[], dogs:[]}
+  end 
+
+
+#   def buy_cat_dog_fish(name) 
+#   cats.new
+#   @cat = cats 
+#   .Owner
+#   .mood=
+#     dogs.new
+  
+#   @dog = dog 
+#   .Owner
+#   .mood=
+  
+#   fish.new 
+#   @fish = fish 
+#   .Owner
+#   .mood=
+  
+  
+#   end 
+  
+# end 
+end 
