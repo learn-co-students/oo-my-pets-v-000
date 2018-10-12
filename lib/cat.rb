745756b3d9d78a00c2dde5ac69aef3ee242b3853
J Fern <jfern20@gmail.com>
2018-10-12 05:07:34 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
 class Cat
-  # code goes here
+    attr_reader :name
+    attr_accessor :nervous    #can't change its name
+       #can change its mood 
+   
+   def initialize(name, mood = nervous )
+     @name = name 
+     @nervous = nervous 
+     
+   end 
+   
 end
