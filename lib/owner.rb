75fa24caa433a3d1c1d45dcb4a2a429c1afcc971
J Fern <jfern20@gmail.com>
2018-10-13 19:34:16 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,27 @@
+require 'pry'
+
 class Owner
-   
-  def initialize(owner)
-  @owner = owner     
-    
+  
+  attr_accessor :name 
+  
+  @@all= []
+  @@count = 0 
+  
+  def initialize(name)
+  @name = name     
+  @@all << self 
+  @@count += 1 
   end 
   
+  def self.all 
+    @@all
+  end 
+  
+  def self.reset_all 
+    @@count 
+    #binding.pry 
+  end 
+  
+
   
 end
