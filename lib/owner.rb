6408b7e0d96afea388102e362c95900cf0ace531
J Fern <jfern20@gmail.com>
2018-10-15 04:28:45 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,26 +2,35 @@ require 'pry'
 
 class Owner
   
-  attr_accessor :name 
+  attr_accessor :name
+  attr_reader :species 
   
   @@all= []
   @@count = 0 
   
-  def initialize(name)
-  @name = name     
+  def initialize(name, species="human")
+  @name = name   
+  @species = species 
   @@all << self 
-  @@count += 1 
+  
   end 
   
   def self.all 
     @@all
-  end 
+  end
   
-  def self.reset_all 
-    @@count 
-    #binding.pry 
-  end 
+    def self.count
+   @@count += 1 
+ 
+   end 
+   
+   def self.reset_all 
+   @@count 
+   end 
+   
   
+ 
+   
 
   
 end
