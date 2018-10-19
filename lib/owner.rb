4d6d1be50f893da5b12c9bf1da3dd348493fb340
Andrew Bernier <a6bernier@gmail.com>
2018-10-19 15:05:10 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,29 @@
+require "pry"
+
 class Owner
-  # code goes here
+
+  attr_accessor :pets, :owners
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(pets, species, owners)
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+    @species = "human"
+    @owners = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.count
+  end
+
 end
