d64d7d1f513142e2936f4d4f78dd3f1bb701d424
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-12 16:09:38 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,30 @@
 class Owner
-  # code goes here
+  @@all = []
+  #attr_reader :species
+  attr_accessor :pets, :species
+  
+  def initialize(owner)
+  @@all << self
+  @pets = {fishes: [], cats: [], dogs: []}
+  
+  end
+
+  def self.all
+    @@all
+  end
+  
+  def self.reset_all
+    @@all.clear
+  end
+    
+  def self.count
+    @@all.count
+  end
+  
+  def species(species)
+    self.species = species
+  end
+  
+  
+  
 end
