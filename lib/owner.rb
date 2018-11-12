33bbec193e35833f0742b031395025f6f6126498
Paul Fell-Cinoman <swellpf@gmail.com>
2018-11-12 16:32:45 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,12 +1,14 @@
+require 'pry'
+
 class Owner
   @@all = []
-  #attr_reader :species
-  attr_accessor :pets, :species
+  attr_reader :species
+  attr_accessor :pets, :name
   
   def initialize(owner)
   @@all << self
   @pets = {fishes: [], cats: [], dogs: []}
-  
+  @species = "human"
   end
 
   def self.all
@@ -21,10 +23,12 @@ class Owner
     @@all.count
   end
   
-  def species(species)
-    self.species = species
+  def say_species
+    message = "I am a #{self.species}."
+    message
   end
   
-  
-  
+  def buy_fish
+    
+  end
 end
