d375f9e9c389b5e2d1072d19d03717bd7a08866e
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-12-24 06:37:13 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Owner
 
   attr_accessor :name, :pets
@@ -30,6 +32,49 @@ class Owner
   
   def say_species
     "I am a #{species}."
+  end
+  
+  def buy_fish(fishName)
+    newFish = Fish.new(fishName)
+    @pets[:fishes] << newFish
+  end
+
+  def buy_cat(catName)
+    newCat = Cat.new(catName)
+    @pets[:cats] << newCat
+  end
+
+  def buy_dog(dogName)
+    newDog = Dog.new(dogName)
+    @pets[:dogs] << newDog
+  end
+  
+  def change_mood(hash, emotion)
+    @pets[hash].map { |animal| animal.mood = emotion }
+  end
+  
+  def walk_dogs
+    change_mood(:dogs, "happy")
+  end
+  
+  def play_with_cats
+    change_mood(:cats, "happy")
+  end 
+  
+  def feed_fish
+    change_mood(:fishes, "happy")
+  end
+  
+  def sell_pets
+    change_mood(:dogs, "nervous")
+    change_mood(:fishes, "nervous")
+    change_mood(:cats, "nervous")
+    @pets = {}
+    # binding.pry
+  end
+  
+  def list_pets
+    "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
   end
   
 end
