bb63061e2e4a37adf67c248af2005ecd5aa369ea
Bobbi York <york1670@gmail.com>
2019-01-17 02:06:20 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,12 @@
+require "pry"
+
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+
+  def initialize(name, mood = "nervous")
+    @mood = mood
+    @name = name
+  end
+
 end
