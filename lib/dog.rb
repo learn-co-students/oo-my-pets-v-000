ab27daf00804526327a7e3665a5614747e285fc7
Emily Nicholl Liepe <e.nicholl.liepe@gmail.com>
2019-01-31 00:53:21 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-  # code goes here
+  attr_reader :name
+  attr_accessor :mood
+  
+  def initialize(name)
+    @name = name
+    @mood = mood
+  end
+  
+  def mood
+    @mood || "nervous"
+  end
+  
 end
