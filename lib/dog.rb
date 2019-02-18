e852fc4e73fbc07856d2e82eb8e1985bb7e3552f
Niusha H <niusha.hadziomerovic@gmail.com>
2019-02-18 20:42:56 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,12 @@
 class Dog
-  # code goes here
+  attr_accessor :mood, :owner
+  attr_writer
+  attr_reader :name
+  
+  def initialize(name, mood = "nervous")
+    @name = name
+    @mood = mood
+  end
+
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+  attr_accessor :name
+  attr_writer
+  attr_reader
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end 
+
+  def self.count 
+    @@all.length
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+
+  
+
 end
