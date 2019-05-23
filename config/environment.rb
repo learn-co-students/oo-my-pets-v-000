87d5ca2c089b69f3c0672fea57c04693166ee5ef
Dan Suarez <suarez.daniel101@gmail.com>
2019-05-23 04:09:55 +0000

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,6 @@
 # Dir[File.join(File.dirname(__FILE__), "../lib", "*.rb")].each {|f| require f}
 
 
-Dir[File.expand_path(File.join(File.dirname(__FILE__), "../lib", "*.rb"))].each do |file|
-        require file
-    end
+# Dir[File.expand_path(File.join(File.dirname(__FILE__), "../lib", "*.rb"))].each do |file|
+#         require file
+#     end
