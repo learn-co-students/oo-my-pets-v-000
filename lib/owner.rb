0c715859174d7dd9d5421200334516d000367a21
Dan Suarez <suarez.daniel101@gmail.com>
2019-05-24 22:47:11 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,96 +63,14 @@ class Owner
     pets[:fishes][0].mood=("happy")
   end
 
-def sell_pets
-  # petsale = []
-  buy_cat(name)
-  # # petsale << c
-  #
-  buy_dog(name)
-  # # petsale << d
-  #
-  buy_fish(name)
-  # petsale << f
+  def sell_pets
+    # pets.each { |key, value| binding.pry [key].mood=("nervous") }
 
-  # petsale
-  # pets[:dogs][0].mood=("happy")
-  pets.each { |key, value| [value].mood=("happy") }
-  # # if pets[:dogs]
-  #   walk_dogs
-  # # elsif pets[:cats]
-  #   play_with_cats
-  # # else
-  #   feed_fish
-  # # end
-  #
-  # pets.each { |key, value| [value].map { |o| o.mood="happy" } }
-
-  # sellpets.each {|o| o.mood = "happy" }
-    # pets
-    # pets.each { |o| o.mood=("nervous") }
-
-    # self.pets.clear
-    # self.pets.each { |o| [o].mood=("nervous") }
-  #   # pets[:dogs].mood=("nervous")
-  #   # pets[:cats].mood=("nervous")
-  #   # pets[:fishes].mood=("nervous")
-  #
-  #   # pets[:dogs][0].mood=("nervous")
-    # pets.clear
-    # pets.select { |key, value| [value] }
-
-
-    # pets.each { |key, value| [key][0].mood=("nervous") }
-  #   # fido = Dog.new("Fido")
-  #   # tabby = Cat.new("Tabby")
-  #   # nemo = Fish.new("Nemo")
-  #   # pets = [fido, tabby, nemo]
-  #
-  #   # pets.each { |type, pets| pets.clear }
-  #   pets.clear
-  #   # pets.rassoc(pets)
-  #   # h.rassoc(obj)
-    # pets.each do |type, pets|
-    #   [:type].mood=("nervous")
-    # end
-  #
-  #   # owner.pets = {
-  #   #   :dogs => [fido, Dog.new("Daisy")],
-  #   #   :fishes => [nemo],
-  #   #   :cats => [Cat.new("Mittens"), tabby]
-  #   # }
-  #
-  #
-    # pets.each do |key, value|
-    #   a = [key].values
-    #   a.mood=("nervous")
-    #   end
-
-      # pets.values_at { |key, value| key.mood = "nervous" }
-    # pets.select { |key, value| [key].values_at(0, 0, 1).map { |o| o.mood = "nervous" } }
-
-    # choices = pets.select { |key, value| key.to_s.match(/^choice\d+/) }
-
-    # pets.each { |o| o.mood="happy" }
-    # pets
-
-
-    # pets.values.map { |key, value| [value].mood=("nervous") }
-    # pets.clear
-    # a = pets.values
-    # a.values_at(0, 0, 1)
-    # a.map { |o| o.mood = "nervous" }
-
-  #
-  #   # a.values_at(i1, i2, i3)
-  #
-    # pets.each { |type, pets| pets[:type].each { |o| o.mood=("nervous") } }
-  #
-    # pets.each { |type, pet| pet.mood=("nervous") }
-  #
-    # pets[:dogs].mood=("nervous")
-    # pets.each { |o| o.mood=("nervous") }
+    pets.each do |species, animals|
+  # binding.pry
+  animals.map { |o| o.mood=("nervous") }
+  animals.clear
+end
   end
 
-  binding.pry
 end
