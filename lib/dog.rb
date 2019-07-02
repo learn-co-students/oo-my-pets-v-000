2318fca2cfb3c2bd5ec598d3306a5b4d53ce5cb4
Jamal Gelin <gelin.jamal@gmail.com>
2019-07-02 20:34:38 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,3 @@
 class Dog
-  # code goes here
-  attr_accessor :mood
-  attr_reader :name
 
-  def initialize
-    @name = name
-    @mood = "excited"
-  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,7 @@
 class Owner
   # code goes here
+  attr_reader :species
+  attr_accessor :name, :pets
+
+  @@all = []
 end
