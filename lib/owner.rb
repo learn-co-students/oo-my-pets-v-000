191e32355a1a76b7627b7947884f2a314f448b78
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-01 23:36:25 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,24 @@
+
+require 'pry'
+
 class Owner
   # code goes here
+
+attr_reader :name, :species
+
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+      #binding.pry
+  end
+
+
+
+  def say_species
+    puts @species
+  end
+
 end
