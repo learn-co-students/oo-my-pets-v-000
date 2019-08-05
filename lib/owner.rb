1da574c03d53227553123dc84e46127cd7987c7b
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-05 16:32:42 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,12 +2,17 @@
 require 'pry'
 
 class Owner
-  # code goes here
 
-attr_reader :name, :species
+  attr_reader :name, :species
+
+  @@all = []
 
   def initialize(name)
     @name = name
+    @species = "human"
+    @@all << self
+    @@cats = []
+    @@dogs = []
   end
 
   def name
@@ -15,13 +20,55 @@ attr_reader :name, :species
       #binding.pry
   end
 
-def species
-  @species = species
-end
-
-
   def say_species
-    puts @species
+  return "I am a #{@species}."
   end
+
+  def self.all
+    @@all
+  end
+
+  def self.count
+    @@all.count
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def cats
+    Cat.all.select { |cat| cat.owner == self}
+  end
+
+  def all_cats
+    @@cats
+    end
+
+    def buy_cat(name)
+      Cat.new(name, self)
+    end
+
+    def dogs
+      Dog.all.select{ |dog| dog.owner == self}
+    end
+
+    def all_dogs
+      @@dogs
+    end
+
+    def buy_dog(name)
+      Dog.new(name, self)
+    end
+
+
+    def walk_dogs
+      @@dogs.each do |dog|
+        dog.each do |e|
+          if dog.mood != happy then dog.mood = "happy"
+        end
+      end
+    end
+
+    end
 
 end
