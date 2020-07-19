96eae7d80ffefc87e90dad206713c98f710b91e8
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2020-07-19 00:08:42 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,24 @@
 class Dog
-  # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@dogs = []
+
+# instance methods
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@dogs << self
+    owner.dogs << self
+  end
+
+# class methods
+
+  def self.all()
+    @@dogs
+  end
+
 end
