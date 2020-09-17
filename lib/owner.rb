1a96a07916529ee8ff82978c91b36e817dfdcd1a
CharlesOK <okwechimeamechi@gmail.com>
2020-09-17 13:01:47 -0400

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -44,7 +44,7 @@ class Owner
       end
      pets.clear
     end
-  end
+  end                 
 
   def list_pets
  "I have #{pets[:fishes].count} fish, #{pets[:dogs].count} dog(s), and #{pets[:cats].count} cat(s)."
