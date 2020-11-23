562481fd2ff131983f8afffcab96a8d4280e28f3
andy-aguilar <aaguil3@gmail.com>
2020-11-23 07:28:00 -0600

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.find_by_name(name)
+    self.all.find{|cat| cat.name == name}
+  end
+  
 end
